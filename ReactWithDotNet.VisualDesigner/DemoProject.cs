5f053ddb9db18735c9dcda138d77f59f4aa26aa2
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-04-08 18:50:30 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/DemoProject.cs
@@ -0,0 +1,22 @@
+﻿using React;
+
+namespace React
+{
+    class ReactNode
+    {
+        
+    }
+}
+namespace DemoProject
+{
+    class CircleImageButtonProps
+    {
+        public bool isDisabled;
+
+        public Action onClick;
+
+        public int size = 32;
+
+        public ReactNode children;
+    }
+}
