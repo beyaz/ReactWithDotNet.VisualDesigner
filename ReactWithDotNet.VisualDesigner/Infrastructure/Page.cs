aaf94c038fbb230afa1b2ce495470557ed7963a7
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-04-23 17:57:12 +0300

    .

--- ReactWithDotNet.VisualDesigner/Infrastructure/Page.cs
+++ ReactWithDotNet.VisualDesigner/Infrastructure/Page.cs
@@ -1,17 +1,9 @@
-
 namespace ReactWithDotNet.WebSite;
 
 sealed record PageRouteInfo(string Url, Type page);
 
 static class Page
 {
-   
-   
-    
-  
-    
     public static readonly PageRouteInfo VisualDesigner = new($"/{nameof(VisualDesigner)}", typeof(ApplicationView));
     public static readonly PageRouteInfo VisualDesignerPreview = new($"/{nameof(VisualDesignerPreview)}", typeof(ApplicationPreview));
-    
-    
 }
