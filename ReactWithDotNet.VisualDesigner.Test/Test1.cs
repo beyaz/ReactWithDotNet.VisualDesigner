a9ca7a1488169451bd2b9272c129eef28596e6e7
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-01 20:21:14 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace ReactWithDotNet.VisualDesigner.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
