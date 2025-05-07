a7e34c0f4c8e93b08fdf67b1372bda58a0fda4ea
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-08 01:26:48 +0300

    .

--- ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
+++ ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
@@ -1,4 +1,4 @@
-﻿using ReactWithDotNet.VisualDesigner.Exporters;
+﻿using static ReactWithDotNet.VisualDesigner.Exporters.NextJs_with_Tailwind;
 
 namespace ReactWithDotNet.VisualDesigner.Test;
 
@@ -8,6 +8,13 @@ public sealed class NextJsExportTest
     [TestMethod]
     public async Task ExportAll()
     {
-        await NextJs_with_Tailwind.ExportAll(1);
+        // await NextJs_with_Tailwind.ExportAll(1);
+        
+        var components = await GetAllComponentsInProject(1);
+
+        foreach (var component in components)
+        {
+            await Export(component.AsExportInput());
+        }
     }
 }
