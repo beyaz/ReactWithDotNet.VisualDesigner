dc6facdd91c73f924d1b6d2543ffa5ea9ecea2e2
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-11 21:13:28 +0300

    .

--- ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
+++ ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
@@ -15,38 +15,38 @@ public sealed class NextJsExportTest
     [TestMethod]
     public async Task FixAll()
     {
-        await DbOperation(async db =>
-        {
-            foreach (var record in db.GetAll<ComponentEntity>())
-            {
-                var root = DeserializeFromYaml<VisualElementModel>(record.RootElementAsYaml);
+        //await DbOperation(async db =>
+        //{
+        //    foreach (var record in db.GetAll<ComponentEntity>())
+        //    {
+        //        var root = DeserializeFromYaml<VisualElementModel>(record.RootElementAsYaml);
 
-                await fix(root);
+        //        await fix(root);
 
-                await db.UpdateAsync(record with
-                {
-                    RootElementAsYaml = SerializeToYaml(root)
-                });
-            }
-        });
+        //        await db.UpdateAsync(record with
+        //        {
+        //            RootElementAsYaml = SerializeToYaml(root)
+        //        });
+        //    }
+        //});
 
-        static async Task fix(VisualElementModel model)
-        {
-            var cmp = await TryFindComponentByComponentName(1, model.Tag);
-            if (cmp.HasError)
-            {
-                throw cmp.Error;
-            }
+        //static async Task fix(VisualElementModel model)
+        //{
+        //    var cmp = await TryFindComponentByComponentName(1, model.Tag);
+        //    if (cmp.HasError)
+        //    {
+        //        throw cmp.Error;
+        //    }
 
-            if (cmp.Value is not null)
-            {
-                model.Tag = cmp.Value.Id.ToString();
-            }
+        //    if (cmp.Value is not null)
+        //    {
+        //        model.Tag = cmp.Value.Id.ToString();
+        //    }
 
-            foreach (var child in model.Children)
-            {
-                await fix(child);
-            }
-        }
+        //    foreach (var child in model.Children)
+        //    {
+        //        await fix(child);
+        //    }
+        //}
     }
 }
