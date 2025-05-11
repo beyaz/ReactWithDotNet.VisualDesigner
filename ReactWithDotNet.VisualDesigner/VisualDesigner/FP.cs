1a378cb464c38004ee3a733cd8230e4f38f17c05
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-11 13:26:29 +0300

    .

--- ReactWithDotNet.VisualDesigner/VisualDesigner/FP.cs
+++ ReactWithDotNet.VisualDesigner/VisualDesigner/FP.cs
@@ -43,6 +43,11 @@ public sealed class Response<TValue>:   IEnumerable<TValue>
         return new() { Value = value, Success = true };
     }
 
+    public static implicit operator Response<TValue>(NoneObject noneObject)
+    {
+        return new() { Success = true };
+    }
+    
     public static implicit operator Response<TValue>(Exception failInfo)
     {
         return new() { Error = failInfo };
@@ -60,42 +65,7 @@ public sealed class Response<TValue>:   IEnumerable<TValue>
     }
 }
 
-public sealed class NullableResponse<TValue>: IEnumerable<TValue>
-{
-    public Exception Error { get; init; }
 
-    public bool HasError => !Success;
-
-    public bool Success { get; init; }
-
-    public TValue Value { get; init; }
-
-    public static implicit operator NullableResponse<TValue>(TValue value)
-    {
-        return new() { Value = value, Success = true };
-    }
-
-    public static implicit operator NullableResponse<TValue>(Exception failInfo)
-    {
-        return new() { Error = failInfo };
-    }
-    
-    public static implicit operator NullableResponse<TValue>(NoneObject noneObject)
-    {
-        return new() { Success = true };
-    }
-    
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
-
-    public IEnumerator<TValue> GetEnumerator()
-    {
-        if (Success)
-            yield return Value;
-    }
-}
 
 public class Result<TValue>
 {
