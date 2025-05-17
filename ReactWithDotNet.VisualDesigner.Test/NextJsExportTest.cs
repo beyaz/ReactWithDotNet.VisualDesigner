426f3e8ae387f9a77d637a2c3f971ab1aa30fcae
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-17 11:37:00 +0300

    .

--- ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
+++ ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
@@ -15,38 +15,53 @@ public sealed class NextJsExportTest
     [TestMethod]
     public async Task FixAll()
     {
-        //await DbOperation(async db =>
-        //{
-        //    foreach (var record in db.GetAll<ComponentEntity>())
-        //    {
-        //        var root = DeserializeFromYaml<VisualElementModel>(record.RootElementAsYaml);
+        await DbOperation(async db =>
+        {
+            foreach (var record in db.GetAll<ComponentEntity>())
+            {
+                var root = DeserializeFromYaml<VisualElementModel>(record.RootElementAsYaml);
 
-        //        await fix(root);
+                await fix(root);
 
-        //        await db.UpdateAsync(record with
-        //        {
-        //            RootElementAsYaml = SerializeToYaml(root)
-        //        });
-        //    }
-        //});
+                await db.UpdateAsync(record with
+                {
+                    RootElementAsYaml = SerializeToYaml(root)
+                });
+            }
+        });
+        return;
 
-        //static async Task fix(VisualElementModel model)
-        //{
-        //    var cmp = await TryFindComponentByComponentName(1, model.Tag);
-        //    if (cmp.HasError)
-        //    {
-        //        throw cmp.Error;
-        //    }
+        static async Task fix(VisualElementModel model)
+        {
+            for (var i = 0; i < model.Styles.Count; i++)
+            {
+                model.Styles[i] = fixStyle(model.Styles[i]);
+            }
 
-        //    if (cmp.Value is not null)
-        //    {
-        //        model.Tag = cmp.Value.Id.ToString();
-        //    }
+            foreach (var child in model.Children)
+            {
+                await fix(child);
+            }
 
-        //    foreach (var child in model.Children)
-        //    {
-        //        await fix(child);
-        //    }
-        //}
+            return;
+
+            static string fixStyle(string style)
+            {
+                var map = new Dictionary<string, string>
+                {
+                    { "-L", "-l" }
+                };
+
+                foreach (var (key, value) in map)
+                {
+                    if (style.EndsWith(key))
+                    {
+                        return style.RemoveFromEnd(key) + value;
+                    }
+                }
+
+                return style;
+            }
+        }
     }
 }
