a4cef5f7329688e439f4f4943ccf9937bef91c96
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-21 15:12:06 +0300

    .

--- ReactWithDotNet.VisualDesigner.Test/HtmlImporterTest.cs
+++ ReactWithDotNet.VisualDesigner.Test/HtmlImporterTest.cs
@@ -8,7 +8,6 @@ public sealed class HtmlImporterTest
     [TestMethod]
     public void ShouldImportWithCssClasses()
     {
-
         var html = """
                    <div style="color: #222530; font-size: 16px; font-family: Host Grotesk; font-weight: 400; line-height: 24px; word-wrap: break-word">Phuket, Thailand</div>
                    """;
@@ -22,14 +21,12 @@ public sealed class HtmlImporterTest
         };
 
         var model = HtmlImporter.ConvertToVisualElementModel(project, html);
-        model.Styles.Should().AllBeEquivalentTo(new string[]
+
+        model.Styles.Should().BeEquivalentTo(new List<string>
         {
             "color: #222530",
             "word-wrap: break-word",
             "ABC"
         });
-
-
-
     }
 }
