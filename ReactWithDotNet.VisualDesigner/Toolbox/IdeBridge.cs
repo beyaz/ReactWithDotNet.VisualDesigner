cacce5b5b1cb87900fac2481330f79143cbe1445
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-26 00:34:47 +0300

    .

--- ReactWithDotNet.VisualDesigner/Toolbox/IdeBridge.cs
+++ ReactWithDotNet.VisualDesigner/Toolbox/IdeBridge.cs
@@ -4,21 +4,65 @@ namespace ReactWithDotNet.VisualDesigner.Toolbox;
 
 static class IdeBridge
 {
-    public static void OpenWebStormEditor(string filePath, int lineNumber)
+    public static Exception OpenWebStormEditor(string filePath, int lineNumber)
     {
-        var response = FindRunningProcessExecutablePath("webstorm64");
-
-        var webstormPath = response.FilePath;
-
-        var startInfo = new ProcessStartInfo
+        // try webstorm
         {
-            FileName        = webstormPath,
-            Arguments       = $"--line {lineNumber} \"{filePath}\"",
-            UseShellExecute = false,
-            CreateNoWindow  = true
-        };
+            var response = FindRunningProcessExecutablePath("webstorm64");
+            if (response.Success)
+            {
+                try
+                {
+                    var exeFilePath = response.FilePath;
 
-        Process.Start(startInfo);
+                    var startInfo = new ProcessStartInfo
+                    {
+                        FileName        = exeFilePath,
+                        Arguments       = $"--line {lineNumber} \"{filePath}\"",
+                        UseShellExecute = false,
+                        CreateNoWindow  = true
+                    };
+
+                    Process.Start(startInfo);
+
+                    return null;
+                }
+                catch (Exception exception)
+                {
+                    return exception;
+                }
+            }
+        }
+
+        // try vscode
+        {
+            var response = FindRunningProcessExecutablePath("Code");
+            if (response.Success)
+            {
+                try
+                {
+                    var exeFilePath = response.FilePath;
+
+                    var startInfo = new ProcessStartInfo
+                    {
+                        FileName        = exeFilePath,
+                        Arguments       = $"--goto \"{filePath}:{lineNumber}\"",
+                        UseShellExecute = true,
+                        CreateNoWindow  = true
+                    };
+
+                    Process.Start(startInfo);
+
+                    return null;
+                }
+                catch (Exception exception)
+                {
+                    return exception;
+                }
+            }
+        }
+
+        return new("EditorNotFound");
     }
 
     public static (bool fail, Exception exception) OpenWebStormEditor(string webstormExeFilePath, string filePath, int lineNumber)
@@ -43,7 +87,7 @@ static class IdeBridge
         }
     }
 
-    static (string FilePath, bool HasFail, Exception Exception) FindRunningProcessExecutablePath(string processName)
+    static (string FilePath, bool Success, Exception Exception) FindRunningProcessExecutablePath(string processName)
     {
         var processes = Process.GetProcessesByName(processName);
 
@@ -54,17 +98,17 @@ static class IdeBridge
                 var filePath = processes[0].MainModule?.FileName;
                 if (!string.IsNullOrWhiteSpace(filePath))
                 {
-                    return (filePath, false, null);
+                    return (filePath, true, null);
                 }
 
-                return (null, true, new("FilePathIsEmpty"));
+                return (null, false, new("FilePathIsEmpty"));
             }
             catch (Exception exception)
             {
-                return (null, true, exception);
+                return (null, false, exception);
             }
         }
 
-        return (null, true, new("ProcessNotFound"));
+        return (null, false, new("ProcessNotFound"));
     }
 }
