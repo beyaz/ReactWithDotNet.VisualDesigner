3c012276a3e59d1450579d416f949b3785ad97b5
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-28 23:04:08 +0300

    .

--- ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
+++ ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
@@ -12,64 +12,16 @@ public sealed class NextJsExportTest
         (await NextJs_with_Tailwind.ExportAll(1)).Success.Should().BeTrue();
     }
 
-    [TestMethod]
-    public async Task FixAll()
-    {
-        await DbOperation(async db =>
-        {
-            foreach (var record in db.GetAll<ComponentEntity>())
-            {
+    //[TestMethod]
+    //public async Task FixAll()
+    //{
+    //    await DbOperation(async db =>
+    //    {
+    //        foreach (var record in db.GetAll<ComponentEntity>())
+    //        {
 
-                string outputFilePath = string.Empty;
-                string name = string.Empty;
-                {
-                    var path = record.Name + ".txt";
-
-                    var fileName = Path.GetFileNameWithoutExtension(path);
-                
-                    if (fileName.Contains("."))
-                    {
-                        outputFilePath = Path.GetDirectoryName(path).Replace('\\','/') + "/" + fileName.Split('.', StringSplitOptions.RemoveEmptyEntries).First() + ".tsx";
-                        name           = fileName.Split('.', StringSplitOptions.RemoveEmptyEntries).Last();
-                    }
-                    else
-                    {
-                        name = fileName;
-                        outputFilePath = Path.GetDirectoryName(path).Replace('\\','/') + "/" + fileName + ".tsx";
-                    }
-                }
-                
-                
                
-                
-
-                await db.UpdateAsync(record with
-                {
-                    ConfigAsYaml = SerializeToYaml(new Dictionary<string,string>
-                    {
-                        {"name", name},
-                        {"exportFilePath", outputFilePath}
-                    })
-                });
-            }
-        });
-        return;
-
-        static async Task fix(VisualElementModel model)
-        {
-            var index = model.Styles.IndexOf("border: 1px solid border-sub");
-            if (index >= 0)
-            {
-                model.Styles.Insert(index, "border-sub");
-                model.Styles.Insert(index, "border");
-
-                model.Styles.Remove("border: 1px solid border-sub");
-            }
-
-            foreach (var child in model.Children)
-            {
-                await fix(child);
-            }
-        }
-    }
+    //        }
+    //    });
+    //}
 }
