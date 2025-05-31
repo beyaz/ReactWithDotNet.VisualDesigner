fea10d24334946840aac0bd80f991ae36f8af68d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-31 20:51:39 +0300

    .

--- ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
+++ ReactWithDotNet.VisualDesigner.Test/NextJsExportTest.cs
@@ -12,26 +12,4 @@ public sealed class NextJsExportTest
         (await NextJs_with_Tailwind.ExportAll(1)).Success.Should().BeTrue();
     }
 
-    [TestMethod]
-    public async Task FixAll()
-    {
-        foreach (var record in await Store.GetAllComponentsInProject(1))
-        {
-            var map = DeserializeFromYaml<Dictionary<string, string>>(record.ConfigAsYaml);
-
-            var name = map["name"];
-            
-            var exportFilePath = map["exportFilePath"];
-
-            await Store.Update(record with
-            {
-                ConfigAsYaml = SerializeToYaml(new Dictionary<string, string>()
-                {
-                    { "Name", name },
-                    { "ExportFilePath", exportFilePath }
-                })
-            });
-
-        }
-    }
 }
