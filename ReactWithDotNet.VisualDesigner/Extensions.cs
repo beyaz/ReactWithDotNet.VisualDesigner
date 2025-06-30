159d03f508a5c8b89a05ee2af8e19b664252549a
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-06-30 23:24:56 +0300

    .

--- ReactWithDotNet.VisualDesigner/Extensions.cs
+++ ReactWithDotNet.VisualDesigner/Extensions.cs
@@ -621,45 +621,4 @@ static class Extensions
 
         return None;
     }
-
-    public class IO
-    {
-        public static async Task<Result<string>> TryReadFile(string filePath)
-        {
-            try
-            {
-                return await File.ReadAllTextAsync(filePath);
-            }
-            catch (Exception exception)
-            {
-                return exception;
-            }
-        }
-
-        public static async Task<Result<string[]>> TryReadFileAllLines(string filePath)
-        {
-            try
-            {
-                return await File.ReadAllLinesAsync(filePath);
-            }
-            catch (Exception exception)
-            {
-                return exception;
-            }
-        }
-
-        public static async Task<Result> TryWriteToFile(string filePath, string fileContent)
-        {
-            try
-            {
-                await File.WriteAllTextAsync(filePath, fileContent);
-            }
-            catch (Exception exception)
-            {
-                return exception;
-            }
-
-            return Success;
-        }
-    }
 }
---
+++ ReactWithDotNet.VisualDesigner/IO.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace ReactWithDotNet.VisualDesigner;
+
+static class IO
+{
+    public static async Task<Result<string>> TryReadFile(string filePath)
+    {
+        try
+        {
+            return await File.ReadAllTextAsync(filePath);
+        }
+        catch (Exception exception)
+        {
+            return exception;
+        }
+    }
+
+    public static async Task<Result<string[]>> TryReadFileAllLines(string filePath)
+    {
+        try
+        {
+            return await File.ReadAllLinesAsync(filePath);
+        }
+        catch (Exception exception)
+        {
+            return exception;
+        }
+    }
+
+    public static async Task<Result> TryWriteToFile(string filePath, string fileContent)
+    {
+        try
+        {
+            await File.WriteAllTextAsync(filePath, fileContent);
+        }
+        catch (Exception exception)
+        {
+            return exception;
+        }
+
+        return Success;
+    }
+}
