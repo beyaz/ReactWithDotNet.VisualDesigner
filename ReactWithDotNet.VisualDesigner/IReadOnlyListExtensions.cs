d764d73de686c4effded3aba5732b9c488fe913b
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-06-30 23:30:53 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/IReadOnlyListExtensions.cs
@@ -0,0 +1,12 @@
+﻿global using static ReactWithDotNet.VisualDesigner.IReadOnlyListExtensions;
+using System.Collections.Immutable;
+
+namespace ReactWithDotNet.VisualDesigner;
+
+static class IReadOnlyListExtensions
+{
+    public static IReadOnlyList<T> Add<T>(this IReadOnlyList<T> readOnlyList, T value)
+    {
+        return readOnlyList.ToImmutableList().Add(value);
+    }
+}
