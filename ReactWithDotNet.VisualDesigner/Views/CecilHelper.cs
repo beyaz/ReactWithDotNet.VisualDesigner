29e38a729dd9db349a5a52a763fd612601520499
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-07-06 10:53:18 +0300

    .

--- ReactWithDotNet.VisualDesigner/Views/CecilHelper.cs
+++ ReactWithDotNet.VisualDesigner/Views/CecilHelper.cs
@@ -5,132 +5,80 @@ namespace ReactWithDotNet.VisualDesigner.Views;
 
 static class CecilHelper
 {
-    // ReSharper disable once UnusedParameter.Local
-    static AssemblyDefinition GetAssemblyDefinition(ApplicationState state)
+    public static IReadOnlyList<string> GetPropertyPathList(string assemblyPath, string typeFullName, Func<PropertyDefinition, bool> matchFunc)
     {
-        return AssemblyDefinition.ReadAssembly(typeof(CecilHelper).Assembly.Location);
-    }
-    
-    public static TypeDefinition GetPropsTypeDefinition(ApplicationState state)
-    {
-        var searchTypeName = "todo";
-        
-        var assembly = GetAssemblyDefinition(state);
-        
-        foreach (var moduleDefinition in assembly.Modules)
-        {
-            foreach (var typeDefinition in moduleDefinition.Types)
-            {
-                
-                if (typeDefinition.Name == searchTypeName)
-                {
-                    return typeDefinition;
-                }
-            }
-        }
-
-        return null;
-    }
-    
-    public static TypeDefinition GetStateTypeDefinition(ApplicationState state)
-    {
-        var searchTypeName = "todo";
-        
-        var assembly = GetAssemblyDefinition(state);
-        
-        foreach (var moduleDefinition in assembly.Modules)
-        {
-            foreach (var typeDefinition in moduleDefinition.Types)
-            {
-                if (typeDefinition.Name == searchTypeName)
-                {
-                    return typeDefinition;
-                }
-            }
-        }
-
-        return null;
-    }
-    
-    public static ICollection<FieldDefinition> GetDefinedProps(ApplicationState state)
-    {
-        var typeDefinition = GetPropsTypeDefinition(state);
-
-        if (typeDefinition is null)
+        var assembly = AssemblyDefinition.ReadAssembly(assemblyPath);
+        if (assembly is null)
         {
             return [];
         }
 
-        return typeDefinition.Fields;
-    }
-    
-    static bool IsDelegate(TypeDefinition type)
-    {
-        return type.BaseType != null &&
-               (type.BaseType.FullName == "System.MulticastDelegate" || type.BaseType.FullName == "System.Delegate");
-    }
-    
-    static bool IsDelegate(TypeReference type)
-    {
-        return IsDelegate(type.Resolve());
-    }
-    
-    public static IEnumerable<string> GetPropsSuggestions(TypeDefinition selectedTag, ApplicationState state)
-    {
-        foreach (var targetField in selectedTag.Fields)
-        {
-            if (targetField.FieldType.FullName == typeof(bool).FullName)
-            {
-                foreach (var fieldDefinition in GetDefinedProps(state))
-                {
-                    if (fieldDefinition.FieldType.FullName == typeof(bool).FullName)
-                    {
-                        yield return $"{targetField.Name}: props.{fieldDefinition.Name}";
-                    }
-                }
-            }
-            
-            if (IsDelegate(targetField.FieldType) )
-            {
-                foreach (var fieldDefinition in GetDefinedProps(state))
-                {
-                    if (IsDelegate(fieldDefinition.FieldType))
-                    {
-                        yield return $"{targetField.Name}: props.{fieldDefinition.Name}";
-                    }
-                }
-            }
-        }
-    }
-    
-    public static IEnumerable<string> GetPropsSuggestions(ApplicationState state)
-    {
-        TypeDefinition definition = null;
-        
-        string tag = null;
-        if (state.Selection.VisualElementTreeItemPath.HasValue())
-        {
-            var selectedVisualItem = FindTreeNodeByTreePath(state.ComponentRootElement, state.Selection.VisualElementTreeItemPath);
-
-            tag = selectedVisualItem.Tag;
-        }
-        
-        foreach (var moduleDefinition in GetAssemblyDefinition(state).Modules)
-        {
-            foreach (var typeDefinition in moduleDefinition.Types)
-            {
-                if (typeDefinition.Name == tag || typeDefinition.Name == tag+"Props")
-                {
-                    definition = typeDefinition;
-                    break;
-                }
-            }
-        }
-
-        if (definition is null)
+        var type = assembly.MainModule.GetType(typeFullName);
+        if (type is null)
         {
             return [];
         }
-        return GetPropsSuggestions(definition, state);
+
+        return FindStringProperties(type, string.Empty, matchFunc);
+
+        static IReadOnlyList<string> FindStringProperties(TypeDefinition type, string prefix, Func<PropertyDefinition, bool> matchFunc)
+        {
+            var properties = from p in type.Properties where matchFunc(p) select prefix + p.Name;
+
+            var nestedProperties = from nestedType in type.NestedTypes
+                                   from nestedProperty in FindStringProperties(nestedType, prefix + nestedType.Name + ".", matchFunc)
+                                   select nestedProperty;
+
+            return properties.Concat(nestedProperties).ToList();
+        }
+    }
+
+    public static bool IsBooleanProperty(PropertyDefinition propertyDefinition)
+    {
+        var propertyTypeFullName = propertyDefinition.PropertyType.FullName;
+
+        string[] types =
+        [
+            typeof(bool).FullName
+        ];
+
+        return types.Contains(propertyTypeFullName) ||
+               types.Contains(propertyTypeFullName.RemoveFromStart("System.Nullable`1<").RemoveFromEnd(">"));
+    }
+
+    public static bool IsDateTimeProperty(PropertyDefinition propertyDefinition)
+    {
+        var propertyTypeFullName = propertyDefinition.PropertyType.FullName;
+
+        string[] types =
+        [
+            typeof(DateTime).FullName
+        ];
+
+        return types.Contains(propertyTypeFullName) ||
+               types.Contains(propertyTypeFullName.RemoveFromStart("System.Nullable`1<").RemoveFromEnd(">"));
+    }
+
+    public static bool IsNumberProperty(PropertyDefinition propertyDefinition)
+    {
+        var propertyTypeFullName = propertyDefinition.PropertyType.FullName;
+
+        string[] types =
+        [
+            typeof(sbyte).FullName,
+            typeof(byte).FullName,
+            typeof(short).FullName,
+            typeof(int).FullName,
+            typeof(long).FullName,
+            typeof(decimal).FullName,
+        ];
+
+        return types.Contains(propertyTypeFullName) ||
+               types.Contains(propertyTypeFullName.RemoveFromStart("System.Nullable`1<").RemoveFromEnd(">"));
+    }
+
+    public static bool IsStringProperty(PropertyDefinition propertyDefinition)
+    {
+        return propertyDefinition.PropertyType.FullName == "System.String";
     }
 }
