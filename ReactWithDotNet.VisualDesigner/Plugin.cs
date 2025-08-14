c295dca777c769bc4d2a62f02373ad2eb4c8ebef
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-08-14 12:50:36 +0300

    .

--- ReactWithDotNet.VisualDesigner/Plugin.cs
+++ ReactWithDotNet.VisualDesigner/Plugin.cs
@@ -460,7 +460,7 @@ static class Plugin
             (typeof(BDigitalBox), BDigitalBox.GetPropSuggestions, null),
             (typeof(BAlert), BAlert.GetPropSuggestions, null),
             (typeof(BIcon), BIcon.GetPropSuggestions, null),
-            (typeof(BDigitalMoneyInput), BDigitalMoneyInput.GetPropSuggestions, null),
+            (typeof(BDigitalMoneyInput), BDigitalMoneyInput.GetPropSuggestions, BDigitalMoneyInput.AnalyzeReactNode),
             (typeof(BComboBox), BComboBox.GetPropSuggestions, BComboBox.AnalyzeReactNode),
             (typeof(BDigitalDatepicker), BDigitalDatepicker.GetPropSuggestions, null),
             (typeof(BInput), BInput.GetPropSuggestions, BInput.AnalyzeReactNode),
@@ -1275,50 +1275,7 @@ static class Plugin
             }
         }
 
-        sealed class BDigitalMoneyInput : PluginComponentBase
-        {
-            public string bind { get; set; }
-
-            public bool? currencyVisible { get; set; }
-            public string fec { get; set; }
-
-            public static IReadOnlyList<(string name, string value)> GetPropSuggestions()
-            {
-                return
-                [
-                ];
-            }
-
-            protected override Element render()
-            {
-                var textContent = "Tutar";
-
-                if (bind.HasValue())
-                {
-                    textContent += " | " + bind;
-                }
-
-                return new div(WidthFull, PaddingTop(16), PaddingBottom(8))
-                {
-                    new FlexRow(AlignItemsCenter, PaddingLeftRight(16), Border(1, solid, "#c0c0c0"), BorderRadius(10), Height(58), JustifyContentSpaceBetween)
-                    {
-                        new div(Color(rgba(0, 0, 0, 0.54)), FontSize16, FontWeight400, FontFamily("Roboto, sans-serif"))
-                        {
-                            textContent
-                        },
-
-                        new div { fec ?? "TL" },
-                    },
-                    new FlexRow(JustifyContentSpaceBetween, FontSize12, PaddingLeftRight(14), Color(rgb(158, 158, 158)), LineHeight15)
-                    {
-                        //new div{ helperText},
-                        //new div{ maxLength }
-                    },
-
-                    Id(id), OnClick(onMouseClick)
-                };
-            }
-        }
+       
 
         sealed class BDigitalPlateNumber : PluginComponentBase
         {
@@ -1776,6 +1733,119 @@ static class Plugin
             }
         }
 
+        sealed class BDigitalMoneyInput : PluginComponentBase
+        {
+            [JsTypeInfo(JsType.String)]
+            public string label { get; set; }
+            
+            [JsTypeInfo(JsType.String)]
+            public string value { get; set; }
+            
+            [JsTypeInfo(JsType.Boolean)]
+            public string currencyVisible { get; set; }
+
+            [JsTypeInfo(JsType.String)]
+            public string fec { get; set; }
+
+            [JsTypeInfo(JsType.Function)]
+            public string handleMoneyInputChange { get; set; }
+            
+            public static IReadOnlyList<(string name, string value)> GetPropSuggestions()
+            {
+                return
+                [
+                ];
+            }
+
+            public static ReactNode AnalyzeReactNode(ReactNode node)
+            {
+                if (node.Tag == nameof(BDigitalMoneyInput))
+                {
+                    var valueProp = node.Properties.FirstOrDefault(x => x.Name == nameof(value));
+                    var handleMoneyInputChangeProp = node.Properties.FirstOrDefault(x => x.Name == nameof(handleMoneyInputChange));
+                    if (valueProp is not null)
+                    {
+                        var properties = node.Properties;
+
+                        List<string> lines =
+                        [
+                            "(value: number) =>",
+                            "{",
+                            $"  updateRequest(r => {{ r.{valueProp.Value.RemoveFromStart("request.")} = value; }});"
+                        ];
+
+                        if (handleMoneyInputChangeProp is not null)
+                        {
+                            if (IsAlphaNumeric(handleMoneyInputChangeProp.Value))
+                            {
+                                lines.Add(handleMoneyInputChangeProp.Value + "(value);");
+                            }
+                            else
+                            {
+                                lines.Add(handleMoneyInputChangeProp.Value);
+                            }
+                        }
+
+                        lines.Add("}");
+
+                        if (handleMoneyInputChangeProp is not null)
+                        {
+                            handleMoneyInputChangeProp = handleMoneyInputChangeProp with
+                            {
+                                Value = string.Join(Environment.NewLine, lines)
+                            };
+
+                            properties = properties.SetItem(properties.FindIndex(x => x.Name == handleMoneyInputChangeProp.Name), handleMoneyInputChangeProp);
+                        }
+                        else
+                        {
+                            properties = properties.Add(new ReactProperty
+                            {
+                                Name  = nameof(handleMoneyInputChange),
+                                Value = string.Join(Environment.NewLine, lines)
+                            });
+                        }
+
+                        node = node with { Properties = properties };
+                    }
+
+                    node = AddContextProp(node);
+                }
+
+                return node with { Children = node.Children.Select(AnalyzeReactNode).ToImmutableList() };
+            }
+            
+            protected override Element render()
+            {
+                var textContent = label ?? "Tutar";
+
+                if (value.HasValue())
+                {
+                    textContent += " | " + value;
+                }
+
+                return new div(WidthFull, PaddingTop(16), PaddingBottom(8))
+                {
+                    new FlexRow(AlignItemsCenter, PaddingLeftRight(16), Border(1, solid, "#c0c0c0"), BorderRadius(10), Height(58), JustifyContentSpaceBetween)
+                    {
+                        new div(Color(rgba(0, 0, 0, 0.54)), FontSize16, FontWeight400, FontFamily("Roboto, sans-serif"))
+                        {
+                            textContent
+                        },
+
+                        new div { fec ?? "TL" },
+                    },
+                    new FlexRow(JustifyContentSpaceBetween, FontSize12, PaddingLeftRight(14), Color(rgb(158, 158, 158)), LineHeight15)
+                    {
+                        //new div{ helperText},
+                        //new div{ maxLength }
+                    },
+
+                    Id(id), OnClick(onMouseClick)
+                };
+            }
+        }
+        
         sealed class BPlateNumber : PluginComponentBase
         {
             public string bind { get; set; }
