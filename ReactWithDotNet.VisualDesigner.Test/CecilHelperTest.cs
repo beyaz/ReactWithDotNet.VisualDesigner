8f000d0975f02d62f54f0f0a90cc1639ed66b1c8
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-09-02 12:09:50 +0300

    .

--- ReactWithDotNet.VisualDesigner.Test/CecilHelperTest.cs
+++ ReactWithDotNet.VisualDesigner.Test/CecilHelperTest.cs
@@ -1,16 +1,34 @@
 ﻿namespace ReactWithDotNet.VisualDesigner.Test;
 
+public sealed  class SampleUserInfo
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public SampleAddressInfo Address { get; set; }
+    public List<string> Tags { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsActive { get; set; }
+}
+
+public sealed class SampleAddressInfo
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string ZipCode { get; set; }
+}
+
+
 [TestClass]
 public class CecilHelperTest
 {
     [TestMethod]
     public void GetPropertyPathList()
     {
-        var assemblyFilePath = @"D:\work\BOA.BusinessModules\Dev\BOA.MobilePos\API\BOA.POSPortal.MobilePos.API\bin\Debug\net8.0\BOA.POSPortal.MobilePos.API.dll";
+        var assemblyFilePath = typeof(CecilHelperTest).Assembly.Location;
 
-        var dotnetTypeFullName = "BOA.POSPortal.MobilePos.API.Types.TestFormClientRequest";
+        var dotnetTypeFullName = typeof(SampleUserInfo).FullName;
 
-        var variableName = "request";
+        const string variableName = "request";
 
         var propertyPathList = CecilHelper.GetPropertyPathList(assemblyFilePath, dotnetTypeFullName, $"{variableName}.", CecilHelper.IsString);
         
