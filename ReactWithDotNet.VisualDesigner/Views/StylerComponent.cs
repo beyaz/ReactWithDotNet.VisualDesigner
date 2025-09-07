b92fe46b509e5df9fac098a8a434a345f1e09fe1
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-09-07 13:00:12 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/Views/StylerComponent.cs
@@ -0,0 +1,14 @@
+namespace ReactWithDotNet.VisualDesigner.Views;
+
+sealed class StylerComponent : Component<StylerComponent.State>
+{
+    protected override Element render()
+    {
+        return base.render();
+    }
+
+    internal record State
+    {
+        
+    }
+}
