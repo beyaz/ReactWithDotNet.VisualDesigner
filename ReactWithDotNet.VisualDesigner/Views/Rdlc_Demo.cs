c6adc37b972810ae1c73f337aa2c87483ab76fc8
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-09-09 23:20:34 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/Views/Rdlc_Demo.cs
@@ -0,0 +1,19 @@
+﻿namespace ReactWithDotNet.VisualDesigner.Views;
+
+static class Rdlc_Demo
+{
+    
+    class LineCollection : List<string>
+    {
+    }
+
+    static LineCollection GenerateHtml()
+    {
+
+        return
+        [
+
+        ];
+    }
+
+}
