09ebc9a3367f04aff22de176504dac19987f0847
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-09-11 21:17:12 +0300

    .

--- ReactWithDotNet.VisualDesigner.Test/Fixer.cs
+++ ReactWithDotNet.VisualDesigner.Test/Fixer.cs
@@ -1,7 +1,7 @@
 ﻿namespace ReactWithDotNet.VisualDesigner.Test;
 
 [TestClass]
-public  class Fixer
+public class Fixer
 {
     [TestMethod]
     public async Task FixAll()
@@ -20,22 +20,78 @@ public  class Fixer
     static VisualElementModel Fix(VisualElementModel model)
     {
         var styles = model.Styles;
-        
-        for (int i = 0; i < styles.Count; i++)
+
+        for (var i = 0; i < styles.Count; i++)
         {
             var text = styles[i];
             var style = ParseStyleAttribute(text);
-            if (style.Name == "width")
+
+
+            {
+                var name = style.Name;
+
+                var map = new Dictionary<string, string>
+                {
+                    ["p"] = "padding",
+                    ["pt"] = "padding-top",
+                    ["pb"] = "padding-bottom",
+                    ["pl"] = "padding-left",
+                    ["pr"] = "padding-right",
+                    
+                    ["m"]  = "margin",
+                    ["mt"] = "margin-top",
+                    ["mb"] = "margin-bottom",
+                    ["ml"] = "margin-left",
+                    ["mr"] = "margin-right",
+                    
+                    ["w"] = "width",
+                    ["h"] = "height"
+                };
+                
+               
+                
+                if (name.In(["width", "height", "font-size", "gap", "border-radius"]))
+                {
+                    if (double.TryParse(style.Value, out _))
+                    {
+                        styles = styles.SetItem(i, $"{name}: {style.Value.Trim()}px");
+                        continue;
+                    }
+                }
+            }
+            
+
+            
+
+
+            if (style.Name == "px")
             {
                 if (double.TryParse(style.Value, out _))
                 {
-                    styles = styles.SetItem(i, $"width: {style.Value.Trim()}px");
+                    styles = styles.SetItem(i, $"padding-left: {style.Value.Trim()}px");
+                    styles = styles.Insert(i + 1, $"padding-right: {style.Value.Trim()}px");
+                    continue;
                 }
+            }
+
+            if (style.Name == "py")
+            {
+                if (double.TryParse(style.Value, out _))
+                {
+                    styles = styles.SetItem(i, $"padding-top: {style.Value.Trim()}px");
+                    styles = styles.Insert(i + 1, $"padding-bottom: {style.Value.Trim()}px");
+                    continue;
+                }
+            }
+
+            if (double.TryParse(style.Value, out _))
+            {
+                ;
             }
         }
 
         var children = model.Children;
-        
+
         for (var i = 0; i < children.Count; i++)
         {
             children = children.SetItem(i, Fix(children[i]));
