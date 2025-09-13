6c66e55036e05cab19296879bf02973dbe88ca37
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-09-13 21:34:48 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/PropertyDomain/ParsedProperty.cs
@@ -0,0 +1,58 @@
+﻿
+global using static ReactWithDotNet.VisualDesigner.PropertyDomain.ParsedPropertyFactory;
+
+namespace ReactWithDotNet.VisualDesigner.PropertyDomain;
+
+public interface IParsedProperty
+{
+    public string Name { get; }
+
+    public string Value { get; }
+}
+
+static class ParsedPropertyFactory
+{
+    public static Result<IParsedProperty> ParseProperty(string nameValueCombined)
+    {
+        if (string.IsNullOrWhiteSpace(nameValueCombined))
+        {
+            return new ArgumentNullException(nameof(nameValueCombined));
+        }
+
+        if (nameValueCombined.StartsWith("..."))
+        {
+            return new ParsedProperty
+            {
+                Name  = Design.SpreadOperator,
+                Value = nameValueCombined
+            };
+        }
+
+        var colonIndex = nameValueCombined.IndexOf(':');
+        if (colonIndex < 0)
+        {
+            return new ArgumentNullException($"{nameValueCombined} should contains :");
+        }
+
+        var name = nameValueCombined[..colonIndex];
+
+        var value = nameValueCombined[(colonIndex + 1)..].Trim();
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return new ArgumentNullException($"{nameValueCombined} should contains value");
+        }
+
+        return new ParsedProperty
+        {
+            Name  = name.Trim(),
+            Value = value
+        };
+    }
+
+    sealed class ParsedProperty : IParsedProperty
+    {
+        public string Name { get; init; }
+
+        public string Value { get; init; }
+    }
+}
