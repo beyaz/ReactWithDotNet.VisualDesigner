27b647f16978d1133b3ec02e171c3a83d33a0069
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-09-14 16:44:19 +0300

    .

--- ReactWithDotNet.VisualDesigner/Views/Rdlc_Demo.cs
+++ ReactWithDotNet.VisualDesigner/Views/Rdlc_Demo.cs
@@ -8,16 +8,48 @@ static class Rdlc_Demo
     {
         #region Designer Generated Code
 
-        return
-        [
-            $"""new img alt=Logo src={UpLogoDataUrl} style = "width: "177.65px"; height: "81.5547px"; objectFit: "contain"">"""
-        ];
+        return new LineCollection
+            {
+            $"<img alt=Logo src={UpLogoDataUrl} style=\"width: 177.65px; height: 81.5547px; object-fit: contain\">",
+            
+            "<div style=\"display: flex; flex-direction: column; flex: 1; min-width: 300px\">",
+            
+            
+            
+            "    <div style=\"display: flex; align-items: center; margin-bottom: 10px\">",
+            
+            from item in records select new LineCollection
+            {
+            
+                "        <span style=\"font-size: 10.6667px; font-weight: 800; margin-left: auto\">",
+                $"            {item}",
+                "        </span>"
+            
+            }
+            
+            ,
+            
+            "    </div>",
+            
+         
+            
+            
+            "</div>"
+            
+        };
 
         #endregion
     }
 
     class LineCollection : List<string>
     {
+        public void Add(IEnumerable<LineCollection> lineCollections)
+        {
+            foreach (var collection in lineCollections)
+            {
+                AddRange(collection);
+            }
+        }
     }
     
     class AnyRecord
