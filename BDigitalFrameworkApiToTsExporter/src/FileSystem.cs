86a23cba7445fc969d0bd0a7c4c24cac0e72b49d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-09-30 16:02:21 +0300

    .

--- BDigitalFrameworkApiToTsExporter/src/FileSystem.cs
+++ BDigitalFrameworkApiToTsExporter/src/FileSystem.cs
@@ -18,6 +18,42 @@ static class FileSystem
         }
     }
 
+    public static Result<Unit, Exception> Save(FileModel file)
+    {
+        try
+        {
+            var fileInfo = new FileInfo(file.Path);
+
+            if (fileInfo.Exists)
+            {
+                fileInfo.IsReadOnly = false;
+                TfsHelper.CheckoutFileFromTfs(file.Path);
+            }
+
+            File.WriteAllText(file.Path, file.Content, Encoding.UTF8);
+
+            return Unit.Value;
+        }
+        catch (Exception exception)
+        {
+            return exception;
+        }
+    }
+
+    public static Result<Unit, Exception> SaveAll(IEnumerable<FileModel> files)
+    {
+        foreach (var file in files)
+        {
+            var result = Save(file);
+            if (result.HasError)
+            {
+                return result;
+            }
+        }
+
+        return Unit.Value;
+    }
+
     public static Exception? WriteAllText(string filePath, string fileContent)
     {
         try
@@ -40,43 +76,4 @@ static class FileSystem
 
         return null;
     }
-    
-    public static Result<Unit, Exception> Save(FileModel file)
-    {
-        try
-        {
-            var fileInfo = new FileInfo(file.Path);
-
-            if (fileInfo.Exists)
-            {
-                fileInfo.IsReadOnly = false;
-                TfsHelper.CheckoutFileFromTfs(file.Path);
-            }
-
-            File.WriteAllText(file.Path, file.Content, Encoding.UTF8);
-
-            return Unit.Value; 
-        }
-        catch (Exception exception)
-        {
-            return exception; 
-        }
-    }
-    
-    public static Result<Unit, Exception> Save(IEnumerable<FileModel> files)
-    {
-        foreach (var file in files)
-        {
-            var result = Save(file);
-            if (result.HasError)
-            {
-                return result;
-            }
-        }
-        
-        return Unit.Value; 
-    }
-
-   
-
 }
