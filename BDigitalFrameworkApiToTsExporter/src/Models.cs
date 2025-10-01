3d82efb65ece10aae879f3179b740e2d867f764c
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-01 17:13:03 +0300

    .

---
+++ BDigitalFrameworkApiToTsExporter/src/Models.cs
@@ -0,0 +1,15 @@
+﻿namespace BDigitalFrameworkApiToTsExporter;
+
+sealed record TsFieldDefinition
+{
+    public required string Name { get; init; }
+    
+    public required bool IsNullable { get; init; }
+    
+    public required string TypeName { get; init; }
+
+    public override string ToString()
+    {
+        return $"{Name}{(IsNullable ? '?' : string.Empty)} : {TypeName};";
+    }
+}
