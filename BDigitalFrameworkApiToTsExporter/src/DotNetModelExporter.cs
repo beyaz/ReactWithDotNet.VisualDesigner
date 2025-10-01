1d62ec9a06ef440e47ac5c820b2b053ecf168013
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-01 17:56:28 +0300

    .

--- BDigitalFrameworkApiToTsExporter/src/DotNetModelExporter.cs
+++ BDigitalFrameworkApiToTsExporter/src/DotNetModelExporter.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text;
 using Mono.Cecil;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace BDigitalFrameworkApiToTsExporter;
 
@@ -38,20 +37,6 @@ static class DotNetModelExporter
 
             return file;
         }
-    }
-
-    static IReadOnlyList<string> GetTsCodes(TypeDefinition typeDefinition)
-    {
-        
-        return TsOutput.GetTsCode(TsModelCreator.CreatFrom(typeDefinition));
-        
-        
-
-        
-
-       
-
-        
     }
 
     static Result<IEnumerable<FileModel>> CalculateFiles()
@@ -113,6 +98,11 @@ static class DotNetModelExporter
         }
     }
 
+    static IReadOnlyList<string> GetTsCodes(TypeDefinition typeDefinition)
+    {
+        return TsOutput.GetTsCode(TsModelCreator.CreatFrom(typeDefinition));
+    }
+
     static string LinesToString(IReadOnlyList<string> lines)
     {
         var sb = new StringBuilder();
@@ -141,143 +131,5 @@ static class DotNetModelExporter
         }
 
         return sb.ToString();
-    }
-
-    
-}
-
-
-static class TsModelCreator
-{
-    
-     static bool IsImplicitDefinition(PropertyDefinition propertyDefinition)
-        {
-            if (propertyDefinition.PropertyType.FullName == "System.Runtime.Serialization.ExtensionDataObject")
-            {
-                return true;
-            }
-
-            if (propertyDefinition.Name == "EqualityContract")
-            {
-                return true;
-            }
-
-            return propertyDefinition.Name.Contains(".");
-        }
-
-        static string GetTSTypeName(TypeReference typeReference)
-        {
-            if (CecilHelper.IsNullableType(typeReference))
-            {
-                return GetTSTypeName(((GenericInstanceType)typeReference).GenericArguments[0]);
-            }
-
-            if (typeReference.FullName == "System.String")
-            {
-                return "string";
-            }
-
-            if (typeReference.FullName == typeof(short).FullName ||
-                typeReference.FullName == typeof(int).FullName ||
-                typeReference.FullName == typeof(byte).FullName ||
-                typeReference.FullName == typeof(sbyte).FullName ||
-                typeReference.FullName == typeof(short).FullName ||
-                typeReference.FullName == typeof(ushort).FullName ||
-                typeReference.FullName == typeof(double).FullName ||
-                typeReference.FullName == typeof(float).FullName ||
-                typeReference.FullName == typeof(decimal).FullName ||
-                typeReference.FullName == typeof(long).FullName)
-
-            {
-                return "number";
-            }
-
-            if (typeReference.FullName == "System.DateTime")
-            {
-                return "Date";
-            }
-
-            if (typeReference.FullName == "System.Boolean")
-            {
-                return "boolean";
-            }
-
-            if (typeReference.FullName == "System.Object")
-            {
-                return "any";
-            }
-
-            if (typeReference.IsGenericInstance)
-            {
-                var genericInstanceType = (GenericInstanceType)typeReference;
-
-                var isArrayType =
-                    genericInstanceType.GenericArguments.Count == 1 &&
-                    (
-                        typeReference.Name == "Collection`1" ||
-                        typeReference.Name == "List`1" ||
-                        typeReference.Name == "IReadOnlyCollection`1" ||
-                        typeReference.Name == "IReadOnlyList`1"
-                    );
-
-                if (isArrayType)
-                {
-                    var arrayType = genericInstanceType.GenericArguments[0];
-                    return GetTSTypeName(arrayType) + "[]";
-                }
-            }
-
-            return typeReference.Name;
-        }
-        
-        
-    static class TypescriptNaming
-    {
-        static readonly CamelCasePropertyNamesContractResolver CamelCasePropertyNamesContractResolver = new();
-
-        public static string GetResolvedPropertyName(string propertyNameInCSharp)
-        {
-            return CamelCasePropertyNamesContractResolver.GetResolvedPropertyName(propertyNameInCSharp);
-        }
-    }
-    
-    public static TsTypeDefinition CreatFrom(TypeDefinition typeDefinition)
-    {
-        
-        IEnumerable<TsFieldDefinition> fields;
-        if (typeDefinition.IsEnum)
-        {
-            fields = from fieldDefinition in typeDefinition.Fields.Where(f => f.Name != "value__")
-                select new TsFieldDefinition
-                {
-                    Name          = fieldDefinition.Name,
-                    ConstantValue = fieldDefinition.Constant + string.Empty,
-                    IsNullable    = false,
-                    TypeName      = string.Empty
-                };
-        }
-        else
-        {
-            fields =
-                from propertyDefinition in typeDefinition.Properties.Where(p => !IsImplicitDefinition(p))
-                select new TsFieldDefinition
-                {
-                    Name          = TypescriptNaming.GetResolvedPropertyName(propertyDefinition.Name),
-                    IsNullable    = CecilHelper.isNullableProperty(propertyDefinition),
-                    TypeName      = GetTSTypeName(propertyDefinition.PropertyType),
-                    ConstantValue = string.Empty
-                };
-        }
-
-        return new()
-        {
-            Name = typeDefinition.Name,
-            
-            IsEnum = typeDefinition.IsEnum,
-            
-            BaseTypeName = typeDefinition.BaseType.FullName == typeof(object).FullName ? string.Empty : typeDefinition.BaseType.Name,
-            
-            Fields = fields.ToList()
-        };
     }
 }
---
+++ BDigitalFrameworkApiToTsExporter/src/TsModelCreator.cs
@@ -0,0 +1,136 @@
+﻿using Mono.Cecil;
+using Newtonsoft.Json.Serialization;
+
+namespace BDigitalFrameworkApiToTsExporter;
+
+static class TsModelCreator
+{
+    public static TsTypeDefinition CreatFrom(TypeDefinition typeDefinition)
+    {
+        IEnumerable<TsFieldDefinition> fields;
+        if (typeDefinition.IsEnum)
+        {
+            fields = from fieldDefinition in typeDefinition.Fields.Where(f => f.Name != "value__")
+                select new TsFieldDefinition
+                {
+                    Name          = fieldDefinition.Name,
+                    ConstantValue = fieldDefinition.Constant + string.Empty,
+                    IsNullable    = false,
+                    TypeName      = string.Empty
+                };
+        }
+        else
+        {
+            fields =
+                from propertyDefinition in typeDefinition.Properties.Where(p => !IsImplicitDefinition(p))
+                select new TsFieldDefinition
+                {
+                    Name          = TypescriptNaming.GetResolvedPropertyName(propertyDefinition.Name),
+                    IsNullable    = CecilHelper.isNullableProperty(propertyDefinition),
+                    TypeName      = GetTSTypeName(propertyDefinition.PropertyType),
+                    ConstantValue = string.Empty
+                };
+        }
+
+        return new()
+        {
+            Name = typeDefinition.Name,
+
+            IsEnum = typeDefinition.IsEnum,
+
+            BaseTypeName = typeDefinition.BaseType.FullName == typeof(object).FullName ? string.Empty : typeDefinition.BaseType.Name,
+
+            Fields = fields.ToList()
+        };
+    }
+
+    static string GetTSTypeName(TypeReference typeReference)
+    {
+        if (CecilHelper.IsNullableType(typeReference))
+        {
+            return GetTSTypeName(((GenericInstanceType)typeReference).GenericArguments[0]);
+        }
+
+        if (typeReference.FullName == "System.String")
+        {
+            return "string";
+        }
+
+        if (typeReference.FullName == typeof(short).FullName ||
+            typeReference.FullName == typeof(int).FullName ||
+            typeReference.FullName == typeof(byte).FullName ||
+            typeReference.FullName == typeof(sbyte).FullName ||
+            typeReference.FullName == typeof(short).FullName ||
+            typeReference.FullName == typeof(ushort).FullName ||
+            typeReference.FullName == typeof(double).FullName ||
+            typeReference.FullName == typeof(float).FullName ||
+            typeReference.FullName == typeof(decimal).FullName ||
+            typeReference.FullName == typeof(long).FullName)
+
+        {
+            return "number";
+        }
+
+        if (typeReference.FullName == "System.DateTime")
+        {
+            return "Date";
+        }
+
+        if (typeReference.FullName == "System.Boolean")
+        {
+            return "boolean";
+        }
+
+        if (typeReference.FullName == "System.Object")
+        {
+            return "any";
+        }
+
+        if (typeReference.IsGenericInstance)
+        {
+            var genericInstanceType = (GenericInstanceType)typeReference;
+
+            var isArrayType =
+                genericInstanceType.GenericArguments.Count == 1 &&
+                (
+                    typeReference.Name == "Collection`1" ||
+                    typeReference.Name == "List`1" ||
+                    typeReference.Name == "IReadOnlyCollection`1" ||
+                    typeReference.Name == "IReadOnlyList`1"
+                );
+
+            if (isArrayType)
+            {
+                var arrayType = genericInstanceType.GenericArguments[0];
+                return GetTSTypeName(arrayType) + "[]";
+            }
+        }
+
+        return typeReference.Name;
+    }
+
+    static bool IsImplicitDefinition(PropertyDefinition propertyDefinition)
+    {
+        if (propertyDefinition.PropertyType.FullName == "System.Runtime.Serialization.ExtensionDataObject")
+        {
+            return true;
+        }
+
+        if (propertyDefinition.Name == "EqualityContract")
+        {
+            return true;
+        }
+
+        return propertyDefinition.Name.Contains(".");
+    }
+
+    static class TypescriptNaming
+    {
+        static readonly CamelCasePropertyNamesContractResolver CamelCasePropertyNamesContractResolver = new();
+
+        public static string GetResolvedPropertyName(string propertyNameInCSharp)
+        {
+            return CamelCasePropertyNamesContractResolver.GetResolvedPropertyName(propertyNameInCSharp);
+        }
+    }
+}
