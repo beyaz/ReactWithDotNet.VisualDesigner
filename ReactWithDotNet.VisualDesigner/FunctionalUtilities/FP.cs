86312bbbf62e66b7b05597ac2bd5b6fea0f139ee
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-04 18:05:30 +0300

    .

--- ReactWithDotNet.VisualDesigner/FunctionalUtilities/FP.cs
+++ ReactWithDotNet.VisualDesigner/FunctionalUtilities/FP.cs
@@ -9,37 +9,6 @@ public sealed class Unit
     public static readonly Unit Value = new();
 }
 
-public sealed record Result<TValue>
-{
-    public Exception Error { get; init; }
-
-    public bool HasError => !Success;
-
-    public bool Success { get; init; }
-
-    public TValue Value { get; init; }
-
-    public static implicit operator Result<TValue>(TValue value)
-    {
-        return new() { Value = value, Success = true };
-    }
-
-    public static implicit operator Result<TValue>(Exception failInfo)
-    {
-        return new() { Error = failInfo };
-    }
-
-    public static implicit operator Result<TValue>(NoneObject _)
-    {
-        return new() { Success = true };
-    }
-
-    public static implicit operator Result<TValue>(Result<Unit> value)
-    {
-        return new() { Success = value.Success, Error = value.Error };
-    }
-}
-
 public sealed record Maybe<TValue> : IEnumerable<TValue>
 {
     public bool HasNoValue => !HasValue;
---
+++ ReactWithDotNet.VisualDesigner/FunctionalUtilities/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace ReactWithDotNet.VisualDesigner.FunctionalUtilities;
+
+public sealed record Result<TValue>
+{
+    public Exception Error { get; init; }
+
+    public bool HasError => !Success;
+
+    public bool Success { get; init; }
+
+    public TValue Value { get; init; }
+
+    public static implicit operator Result<TValue>(TValue value)
+    {
+        return new() { Value = value, Success = true };
+    }
+
+    public static implicit operator Result<TValue>(Exception failInfo)
+    {
+        return new() { Error = failInfo };
+    }
+
+    public static implicit operator Result<TValue>(NoneObject _)
+    {
+        return new() { Success = true };
+    }
+
+    public static implicit operator Result<TValue>(Result<Unit> value)
+    {
+        return new() { Success = value.Success, Error = value.Error };
+    }
+}
