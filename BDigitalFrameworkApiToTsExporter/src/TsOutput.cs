c1ea120f982cb6508c96d14ef61f5460fcd7ebdc
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-06 16:08:53 +0300

    .

--- BDigitalFrameworkApiToTsExporter/src/TsOutput.cs
+++ BDigitalFrameworkApiToTsExporter/src/TsOutput.cs
@@ -4,15 +4,15 @@ namespace BDigitalFrameworkApiToTsExporter;
 
 static class TsOutput
 {
-    public static  string GetTsCode(TsFieldDefinition field)
+    public static string GetTsCode(TsFieldDefinition field)
     {
         return $"{field.Name}{(field.IsNullable ? '?' : string.Empty)} : {field.TypeName};";
     }
-    
+
     public static IReadOnlyList<string> GetTsCode(TsTypeDefinition type)
     {
         List<string> lines = [];
-        
+
         if (type.IsEnum)
         {
             lines.Add($"export enum {type.Name}");
@@ -33,42 +33,34 @@ static class TsOutput
         }
 
         lines.Add("{");
-        
+
         if (type.IsEnum)
         {
-            var fieldDeclarations = new List<string>();
-            foreach (var field in type.Fields.Where(f => f.Name != "value__"))
-            {
-                fieldDeclarations.Add($"{field.Name} = {field.ConstantValue}");
-            }
-
-            for (var i = 0; i < fieldDeclarations.Count; i++)
-            {
-                var declaration = fieldDeclarations[i];
-
-                if (i < fieldDeclarations.Count - 1)
+            var definitionLines =
+                from field in type.Fields
+                let line = $"{field.Name} = {field.ConstantValue}"
+                let isLast = field == type.Fields[^1]
+                select isLast switch
                 {
-                    lines.Add(declaration + ",");
-                }
-                else
-                {
-                    lines.Add(declaration);
-                }
-            }
+                    true  => line,
+                    false => line + ","
+                };
+            
+            lines.AddRange(definitionLines);
         }
         else
         {
             foreach (var field in type.Fields)
             {
-                lines.Add(field.ToString());
+                lines.Add(GetTsCode(field));
             }
         }
 
         lines.Add("}");
-        
+
         return lines;
     }
-    
+
     public static string LinesToString(IReadOnlyList<string> lines)
     {
         var sb = new StringBuilder();
