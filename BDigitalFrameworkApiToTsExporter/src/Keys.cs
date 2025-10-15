a8b003abf788dc4ef301a56e8c86348d845a2973
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-15 23:33:26 +0300

    .

--- BDigitalFrameworkApiToTsExporter/src/Keys.cs
+++ BDigitalFrameworkApiToTsExporter/src/Keys.cs
@@ -5,9 +5,17 @@ namespace BDigitalFrameworkApiToTsExporter;
 
 static class Keys
 {
-    public static readonly ScopeKey<AssemblyDefinition> Assembly = new() { Key = nameof(AssemblyDefinition) };
+    // @formatter:off
     
     public static readonly ScopeKey<ConfigModel> Config = new() { Key = nameof(Config) };
+        
+    public static readonly ScopeKey<AssemblyDefinition> Assembly = new() { Key = nameof(AssemblyDefinition) };
     
     public static readonly ScopeKey<ApiInfo> Api = new() { Key = nameof(ApiInfo) };
+    
+    public static readonly ScopeKey<TypeDefinition> ModelTypeDefinition = new() { Key = nameof(ModelTypeDefinition) };
+    
+    public static readonly ScopeKey<MethodDefinition> MethodDefinition = new() { Key = nameof(MethodDefinition) };
+    
+    // @formatter:on
 }
