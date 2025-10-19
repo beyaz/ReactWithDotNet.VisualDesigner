96adfccb1d866cebe39cdf9bb32e4f3a80e7cf28
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-19 23:14:24 +0300

    .

--- ReactWithDotNet.VisualDesigner/BDigitalFrameworkApiToTsExporter/MainWindow.cs
+++ ReactWithDotNet.VisualDesigner/BDigitalFrameworkApiToTsExporter/MainWindow.cs
@@ -1,4 +1,5 @@
-﻿using ReactWithDotNet;
+﻿using System.IO;
+using ReactWithDotNet;
 
 namespace BDigitalFrameworkApiToTsExporter;
 
@@ -8,7 +9,7 @@ class MainWindow : Component<MainWindow.Model>
     {
         return new div(WidthFull, HeightFull, BackgroundColor(WhiteSmoke), Padding(16), FontSize13, FontFamily("-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'"))
         {
-            new div(WidthFull, HeightFull, BorderRadius(4), BackgroundColor(White), Border(1, solid, Gray300), DisplayFlex, FlexDirectionColumn)
+            new div(WidthFull, HeightFull, BorderRadius(4), BackgroundColor(White), Border(1, solid, Gray300), DisplayFlex, FlexDirectionColumn, CursorDefault)
             {
                 new div(BorderBottom(1, solid, "#d1d5db"), Padding(8))
                 {
@@ -29,13 +30,14 @@ class MainWindow : Component<MainWindow.Model>
                                 "Api List"
                             }
                         },
-                        new div(Flex(1, 1, 0), Padding(8), DisplayFlex, FlexDirectionColumn, Gap(16))
+                        new div(Flex(1, 1, 0), Padding(8), DisplayFlex, FlexDirectionColumn, Gap(16), OverflowAuto)
                         {
-                            new div(Border(1, solid, Gray300), BorderRadius(4), DisplayFlex, JustifyContentCenter, AlignItemsCenter)
+                            from item in state.ApiNames
+                            select new div(OnClick(OnApiSelected), Id(item), Border(1, solid, Gray300), BorderRadius(4), DisplayFlex, JustifyContentCenter, AlignItemsCenter, state.SelectedApiName == item ? BackgroundColor(Gray100) : BackgroundColor(White), Hover(BorderColor(Gray500)))
                             {
                                 new div(Padding(4))
                                 {
-                                    "Api 1"
+                                    item
                                 }
                             }
                         }
@@ -44,7 +46,7 @@ class MainWindow : Component<MainWindow.Model>
                     {
                         new div(Width(150), BorderRight(1, solid, "#d1d5db"), DisplayFlex, FlexDirectionColumn)
                         {
-                            new div(Height(40), FontWeight600, DisplayFlex, JustifyContentCenter, AlignItemsCenter, BorderBottom(1, solid, "#d1d5db"))
+                            new div(Height(40), FontWeight600, DisplayFlex, JustifyContentCenter, AlignItemsCenter, BorderBottom(1, solid, "#d1d5db")) 
                             {
                                 new div
                                 {
@@ -93,10 +95,12 @@ class MainWindow : Component<MainWindow.Model>
     {
         state = new()
         {
+            StatusMessage = "Ready",
+            
             AssemblyFilePath = @"D:\work\BOA.BusinessModules\Dev\BOA.InternetBanking.Payments\API\BOA.InternetBanking.Payments.API\bin\Debug\net8.0\BOA.InternetBanking.Payments.API.dll"
         };
-        
-        return base.constructor();
+
+        return TryUpdateApiList();
     }
        
     static async Task Main2()
@@ -110,8 +114,46 @@ class MainWindow : Component<MainWindow.Model>
         }
     }
 
+    Task TryUpdateApiList()
+    {
+        if (!File.Exists(state.AssemblyFilePath))
+        {
+            return Task.CompletedTask;
+        }
+
+        var assembly = CecilHelper.ReadAssemblyDefinition(state.AssemblyFilePath);
+        if (assembly.HasError)
+        {
+            state.StatusMessage = assembly.Error.Message;
+            return Task.CompletedTask;
+        }
+
+        var apiTypes = from type in assembly.Value.MainModule.Types where type.BaseType?.Name == "CommonControllerBase" select type;
+        
+        state.ApiNames = from type in apiTypes select type.Name.RemoveFromEnd("Controller");
+        
+        
+        return Task.CompletedTask;
+    }
+     
+    Task OnApiSelected(MouseEvent e)
+    {
+
+        state.SelectedApiName = e.currentTarget.id;
+        
+        return Task.CompletedTask;
+    }
+    
+    
+
     internal record Model
     {
         public string AssemblyFilePath { get; init; }
+        
+        public string StatusMessage { get; set; }
+        
+        public IEnumerable<string> ApiNames { get; set; }
+
+        public string SelectedApiName { get; set; }
     }
 }
