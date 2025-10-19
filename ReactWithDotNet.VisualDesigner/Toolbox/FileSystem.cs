cf3611931bbf6161519c9500f082eb9316c701bf
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-19 19:32:48 +0300

    .

--- ReactWithDotNet.VisualDesigner/Toolbox/FileSystem.cs
+++ ReactWithDotNet.VisualDesigner/Toolbox/FileSystem.cs
@@ -3,6 +3,13 @@ using System.Text;
 
 namespace Toolbox;
 
+public sealed record FileModel
+{
+    public required string Content { get; init; }
+
+    public required string Path { get; init; }
+}
+
 static class FileSystem
 {
     public static async Task<Result<string[]>> ReadAllLines(string filePath)
@@ -16,7 +23,7 @@ static class FileSystem
             return exception;
         }
     }
-
+    
     public static async Task<Result<string>> ReadAllText(string filePath)
     {
         try
@@ -34,47 +41,29 @@ static class FileSystem
         try
         {
             var fileInfo = new FileInfo(file.Path);
-            if (fileInfo.IsReadOnly)
-            {
-                fileInfo.IsReadOnly = false;
-            }
-
-            Encoding encoding;
 
             if (fileInfo.Exists)
             {
-                // Dosyayı açıp encoding tespit et
-                using (var reader = new StreamReader(file.Path, Encoding.UTF8, detectEncodingFromByteOrderMarks: true))
+                // check already same
+                var fileContentInDirectory = await File.ReadAllTextAsync(file.Path);
+                
+                if (IsEqualsIgnoreWhitespace(fileContentInDirectory, file.Content))
                 {
-                    // İlk okumayı yapmazsak CurrentEncoding default (UTF8) kalabilir
-                    var buffer = new char[1];
-                    await reader.ReadAsync(buffer, 0, 1);
-                    encoding = reader.CurrentEncoding;
+                    return Unit.Value;
                 }
-            }
-            else
-            {
-                // Dosya yoksa UTF-8 kullan
-                encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+                    
+                
+                fileInfo.IsReadOnly = false;
+                TfsHelper.CheckoutFileFromTfs(file.Path);
             }
 
-            // Dosyaya mevcut encoding ile yaz
-            await using var writer = new StreamWriter(file.Path, false, encoding);
+            await File.WriteAllTextAsync(file.Path, file.Content, Encoding.UTF8);
 
-            await writer.WriteAsync(file.Content);
+            return Unit.Value;
         }
         catch (Exception exception)
         {
             return exception;
         }
-
-        return Unit.Value;
     }
-}
-
-public sealed record FileModel
-{
-    public required string Content { get; init; }
-
-    public required string Path { get; init; }
 }
---
+++ ReactWithDotNet.VisualDesigner/Toolbox/Scope.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace Toolbox;
+
+public class ScopeKey
+{
+    public required string Key { get; init; }
+
+    public override int GetHashCode()
+    {
+        return Key.GetHashCode();
+    }
+}
+
+public sealed class ScopeKey<T> : ScopeKey
+{
+    public T this[Scope scope]
+    {
+        get
+        {
+            var map = scope.AsDictionary();
+            if (map.TryGetValue(this, out var value))
+            {
+                return (T)value;
+            }
+
+            throw new KeyNotFoundException(Key);
+        }
+    }
+}
+
+public sealed class ScopeCreationInput : IEnumerable
+{
+    readonly Dictionary<ScopeKey, object> items = new();
+
+    public void Add<T>(ScopeKey<T> key, T value)
+    {
+        items.Add(key, value!);
+    }
+
+    public IReadOnlyDictionary<ScopeKey, object> AsDictionary() => items;
+
+    public IEnumerator GetEnumerator()
+    {
+        return items.GetEnumerator();
+    }
+}
+
+[DebuggerTypeProxy(typeof(ScopeDebugView))]
+public sealed class Scope
+{
+    readonly Dictionary<ScopeKey, object> items = new();
+
+    Scope()
+    {
+    }
+
+    public static Scope Create(ScopeCreationInput input)
+    {
+        var scope = new Scope();
+
+        foreach (var (key, value) in input.AsDictionary())
+        {
+            scope.items.Add(key, value);
+        }
+
+        return scope;
+    }
+
+    public IReadOnlyDictionary<ScopeKey, object> AsDictionary() => items;
+
+    public bool Has(ScopeKey key)
+    {
+        return items.ContainsKey(key);
+    }
+
+    public Scope With(ScopeCreationInput input)
+    {
+        var scope = new Scope();
+
+        foreach (var (key, value) in items)
+        {
+            scope.items.Add(key, value);
+        }
+
+        foreach (var (key, value) in input.AsDictionary())
+        {
+            scope.items[key] = value;
+        }
+
+        return scope;
+    }
+    
+    public Scope With<T>(ScopeKey<T> scopeKey, T value )
+    {
+        var scope = new Scope();
+
+        foreach (var item in items)
+        {
+            scope.items.Add(item.Key, item.Value);
+        }
+
+        scope.items[scopeKey] = value!;
+
+        return scope;
+    }
+    
+    
+
+    
+
+    class ScopeDebugView
+    {
+        readonly Scope scope;
+
+        public ScopeDebugView(Scope scope)
+        {
+            this.scope = scope;
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public ItemDebugView[] Items => scope.items.Select(p => new ItemDebugView { Key = p.Key.Key, Value = p.Value }).ToArray();
+    }
+
+    [DebuggerDisplay("{Key} : {Value}")]
+    class ItemDebugView
+    {
+        public required string Key { get; init; }
+        public required object Value { get; init; }
+    }
+}
+
+public static class ScopeExtensions
+{
+    public static Result<Scope> With<T>(this Scope existingScope, ScopeKey<T> scopeKey, Result<T> result)
+    {
+        if (result.HasError)
+        {
+            return result.Error;
+        }
+
+        return existingScope.With(scopeKey, result.Value);
+    }
+    public static Result<Scope> With<T>(this Result<Scope> existingScope, ScopeKey<T> scopeKey, Func<Scope, Result<T>> next)
+    {
+        if (existingScope.HasError)
+        {
+            return existingScope.Error;
+        }
+
+        var result = next(existingScope.Value);
+        
+        if (result.HasError)
+        {
+            return result.Error;
+        }
+
+        return existingScope.Value.With(scopeKey, result.Value);
+    }
+}
