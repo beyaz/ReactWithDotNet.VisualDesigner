cdd6a4f9f281b31f5dc998ff13a3ccc6b13700d1
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-25 19:44:47 +0300

    .

--- ReactWithDotNet.VisualDesigner/Plugin.cs
+++ ReactWithDotNet.VisualDesigner/Plugin.cs
@@ -893,16 +893,3 @@ sealed class NodeAnalyzerAttribute : Attribute
 {
 }
 
-public sealed class TsLineCollection: List<string>
-{
-
-    public void Add(IEnumerable<string> lines)
-    {
-        AddRange(lines);
-    }
-
-    public string ToTsCode()
-    {
-        return string.Join(Environment.NewLine, from line in this where line is not null select line);
-    }
-}
--- ReactWithDotNet.VisualDesigner/Plugins/b-digital/BDigitalGroupView.cs
+++ ReactWithDotNet.VisualDesigner/Plugins/b-digital/BDigitalGroupView.cs
@@ -5,6 +5,19 @@ using ReactWithDotNet.VisualDesigner.Exporters;
 
 namespace ReactWithDotNet.VisualDesigner.Plugins.b_digital;
 
+sealed class TsLineCollection: List<string>
+{
+
+    public void Add(IEnumerable<string> lines)
+    {
+        AddRange(lines);
+    }
+
+    public string ToTsCode()
+    {
+        return string.Join(Environment.NewLine, from line in this where line is not null select line);
+    }
+}
 
 [CustomComponent]
         [Import(Name = nameof(BDigitalTransactionConfirm), Package = "b-digital-transaction-confirm")]
