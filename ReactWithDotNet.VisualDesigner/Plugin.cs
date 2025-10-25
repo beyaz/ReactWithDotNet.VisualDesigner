2a3481cda26076c4a3075d38ecea15a696b1c89f
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-25 18:21:52 +0300

    .

--- ReactWithDotNet.VisualDesigner/Plugin.cs
+++ ReactWithDotNet.VisualDesigner/Plugin.cs
@@ -499,46 +499,14 @@ class Plugin: PluginBase
 
     public static Element TryGetIconForElementTreeNode(VisualElementModel node)
     {
-        {
+        
             var scope = Scope.Create(new()
             {
                 { VisualElementModel, node}
             });
 
-            var el = RunPluginMethods(TryGetIconForElementTreeNodes, scope, IconForElementTreeNode);
-            if (el is not null)
-            {
-                return el;
-            }
-        }
-        
-        
+            return RunPluginMethods(TryGetIconForElementTreeNodes, scope, IconForElementTreeNode);
 
-        if (node.Tag is nameof(Components.BDigitalGrid))
-        {
-            foreach (var p in node.Properties)
-            {
-                foreach (var property in TryParseProperty(p))
-                {
-                    if (property.Name == "direction")
-                    {
-                        if (TryClearStringValue(property.Value).Contains("column", StringComparison.OrdinalIgnoreCase))
-                        {
-                            return new IconFlexColumn();
-                        }
-
-                        if (TryClearStringValue(property.Value).Contains("row", StringComparison.OrdinalIgnoreCase))
-                        {
-                            return new IconFlexRow();
-                        }
-                    }
-                }
-            }
-
-            return new IconFlexRow();
-        }
-
-        return null;
     }
 
     static string ConvertDotNetPathToJsPath(string dotNetPath)
@@ -695,72 +663,7 @@ class Plugin: PluginBase
             }
         }
 
-        [CustomComponent]
-        [Import(Name = "BDigitalGrid", Package = "b-digital-grid")]
-        public sealed class BDigitalGrid : PluginComponentBase
-        {
-            [Suggestions("flex-start , flex-end , stretch , center , baseline")]
-            [JsTypeInfo(JsType.String)]
-            public string alignItems { get; set; }
-
-            [Suggestions("true")]
-            [JsTypeInfo(JsType.Boolean)]
-            public string container { get; set; }
-
-            [Suggestions("column , row")]
-            [JsTypeInfo(JsType.String)]
-            public string direction { get; set; }
-
-            [Suggestions("true")]
-            [JsTypeInfo(JsType.Boolean)]
-            public string item { get; set; }
-
-            [Suggestions("flex-start , center , flex-end , space-between , space-around , space-evenly")]
-            [JsTypeInfo(JsType.String)]
-            public string justifyContent { get; set; }
-
-            [JsTypeInfo(JsType.Number)]
-            public string lg { get; set; }
-
-            [JsTypeInfo(JsType.Number)]
-            public string md { get; set; }
-
-            [JsTypeInfo(JsType.Number)]
-            public string sm { get; set; }
-
-            [Suggestions("1 , 2 , 3 , 4 , 5 , 6")]
-            [JsTypeInfo(JsType.Number)]
-            public string spacing { get; set; }
-
-            [JsTypeInfo(JsType.Number)]
-            public string xl { get; set; }
-
-            [JsTypeInfo(JsType.Number)]
-            public string xs { get; set; }
-
-            protected override Element render()
-            {
-                return new Grid
-                {
-                    children = { children },
-
-                    container      = container == null ? null : Convert.ToBoolean(container),
-                    item           = item == null ? null : Convert.ToBoolean(item),
-                    direction      = direction,
-                    justifyContent = justifyContent,
-                    alignItems     = alignItems,
-                    spacing        = spacing == null ? null : Convert.ToDouble(spacing),
-                    xs             = xs == null ? null : Convert.ToInt32(xs),
-                    sm             = sm == null ? null : Convert.ToInt32(sm),
-                    md             = md == null ? null : Convert.ToInt32(md),
-                    lg             = lg == null ? null : Convert.ToInt32(lg),
-                    xl             = xl == null ? null : Convert.ToInt32(xl),
-
-                    id      = id,
-                    onClick = onMouseClick
-                };
-            }
-        }
+      
 
        
 
