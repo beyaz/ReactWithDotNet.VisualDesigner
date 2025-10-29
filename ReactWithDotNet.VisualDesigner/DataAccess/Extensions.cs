b82da6c4a2ff0e36c4751454ca6950dcf98e2b60
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-29 10:09:55 +0300

    .

--- ReactWithDotNet.VisualDesigner/DataAccess/Extensions.cs
+++ ReactWithDotNet.VisualDesigner/DataAccess/Extensions.cs
@@ -1,14 +1,8 @@
 ﻿global using static ReactWithDotNet.VisualDesigner.DataAccess.Extensions;
-
-
 using System.IO;
 
 namespace ReactWithDotNet.VisualDesigner.DataAccess;
 
-public static class ScopeKeys
-{
-    public static readonly ScopeKey<int> ComponentId = nameof(ComponentId);
-}
 record GetComponentDataInput
 {
     public int ComponentId { get; init; }
@@ -25,22 +19,19 @@ record GetComponentDataOutput
 
 static class Extensions
 {
-    public static string GetRootElementAsYaml(GetComponentDataOutput x)
-    {
-        if (x.WorkspaceVersion.HasValue)
-        {
-            return x.WorkspaceVersion.Value.RootElementAsYaml;
-        }
+    public static readonly ScopeKey<ComponentEntity> Component = nameof(Component);
+    public static readonly ScopeKey<int> ComponentId = nameof(ComponentId);
 
-        return x.Component.RootElementAsYaml;
-    }
-    
+    public static readonly ScopeKey<Maybe<ComponentWorkspace>> ComponentWorkspaceVersion = nameof(ComponentWorkspaceVersion);
+
+    public static readonly ScopeKey<string> UserName = nameof(UserName);
+
     public static async Task<Result<GetComponentDataOutput>> GetComponentData(GetComponentDataInput input)
     {
         var output = new GetComponentDataOutput
         {
             Component        = await Store.TryGetComponent(input.ComponentId),
-            WorkspaceVersion = await Store.TryGetComponentWorkspace(input.ComponentId ,input.UserName)
+            WorkspaceVersion = await Store.TryGetComponentWorkspace(input.ComponentId, input.UserName)
         };
 
         if (output.Component is null)
@@ -50,6 +41,35 @@ static class Extensions
 
         return output;
     }
-    
-    
+
+   
+    public static async Task<Result<Scope>> GetComponentData(Scope scope)
+    {
+        var componentId = ComponentId[scope];
+        var userName = UserName[scope];
+
+        var component = await Store.TryGetComponent(componentId);
+        if (component is null)
+        {
+            return new IOException($"ComponentNotFound.{componentId}");
+        }
+
+        var componentWorkspaceVersion = await Store.TryGetComponentWorkspace(componentId, userName);
+
+        return Scope.Create(new()
+        {
+            { Component, component },
+            { ComponentWorkspaceVersion, componentWorkspaceVersion }
+        });
+    }
+
+    public static string GetRootElementAsYaml(GetComponentDataOutput x)
+    {
+        if (x.WorkspaceVersion.HasValue)
+        {
+            return x.WorkspaceVersion.Value.RootElementAsYaml;
+        }
+
+        return x.Component.RootElementAsYaml;
+    }
 }
