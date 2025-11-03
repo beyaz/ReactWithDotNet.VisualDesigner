f709e82bf8420ae66cf65eaef9650e2b8847e5b1
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-11-03 22:25:33 +0300

    .

--- ReactWithDotNet.VisualDesigner/DbModels/Extensions.cs
+++ ReactWithDotNet.VisualDesigner/DbModels/Extensions.cs
@@ -51,39 +51,15 @@ public static class Extensions
     
     
 
-    public static string GetName(this ComponentEntity componentEntity)
-    {
-        foreach (var name in componentEntity.TryGetName())
-        {
-            return name;
-        }
+    
 
-        throw new InvalidOperationException("name not found");
-    }
+    
 
     
 
     
 
-    public static Maybe<string> TryGetExportFilePath(this ComponentEntity componentEntity)
-    {
-        if (DeserializeFromYaml<Dictionary<string, string>>(componentEntity.ConfigAsYaml).TryGetValue(ComponentConfigReservedName.ExportFilePath, out var value))
-        {
-            return value;
-        }
-
-        return None;
-    }
-
-    public static Maybe<string> TryGetName(this ComponentEntity componentEntity)
-    {
-        if (DeserializeFromYaml<Dictionary<string, string>>(componentEntity.ConfigAsYaml).TryGetValue(ComponentConfigReservedName.Name, out var value))
-        {
-            return value;
-        }
-
-        return None;
-    }
+  
     
    
 }
