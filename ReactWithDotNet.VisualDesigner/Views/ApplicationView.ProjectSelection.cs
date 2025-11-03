07e1132a0386d9e42856cd5651d4c96ffc743485
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-11-04 00:53:54 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/Views/ApplicationView.ProjectSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Text;
+using ReactWithDotNet.ThirdPartyLibraries.MonacoEditorReact;
+using ReactWithDotNet.VisualDesigner.Exporters;
+
+namespace ReactWithDotNet.VisualDesigner.Views;
+
+class ProjectSelectionView : Component<ProjectSelectionView.State>
+{
+    protected override Element render()
+    {
+        return new div(DisplayFlex, AlignItemsCenter, JustifyContentCenter, PositionRelative, Border(1, solid, "#d5d5d8"), BorderRadius(4), Height(36), WidthFitContent)
+        {
+            
+            
+        };
+    }
+
+    internal class State
+    {
+        
+    }
+}
