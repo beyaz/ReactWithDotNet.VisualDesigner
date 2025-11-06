11d641e2a4431a23bf62ba359ce45e0ddebba04e
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-11-06 13:31:30 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/Toolbox/Maybe_Result.cs
@@ -0,0 +1,26 @@
+﻿namespace Toolbox;
+
+public static class MaybeResultExtensions
+{
+    public static Result<Maybe<C>> SelectMany<A, B, C>(
+        this Result<A> source,
+        Func<A, Maybe<B>> bind,
+        Func<A, B, C> resultSelector
+    )
+    {
+        if (source.HasError)
+        {
+            return source.Error;
+        }
+
+        var maybe = bind(source.Value);
+        if (maybe.HasNoValue)
+        {
+            return new() { Value = None };
+        }
+
+        Maybe<C> c = resultSelector(source.Value, maybe.Value);
+
+        return c;
+    }
+}
