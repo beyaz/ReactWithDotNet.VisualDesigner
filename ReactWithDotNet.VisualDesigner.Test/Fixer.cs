eb60f94cf1584c905ba34de79224973a0dadbef9
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-11-08 16:47:57 +0300

    .

--- ReactWithDotNet.VisualDesigner.Test/Fixer.cs
+++ ReactWithDotNet.VisualDesigner.Test/Fixer.cs
@@ -3,51 +3,12 @@
 [TestClass]
 public class Fixer
 {
-    const int ProjectId = 1;
 
     [TestMethod]
-    public async Task FixAll()
+    public void FixAll()
     {
         Configuration.Extensions.Config = Configuration.Extensions.ReadConfig().Value;
 
-        foreach (var component in await Store.GetAllComponentsInProject(ProjectId))
-        {
-            var config = DeserializeFromYaml<ComponentConfig>(component.ConfigAsYaml);
-
-            await Store.Update(component with
-            {
-                ConfigAsYaml = SerializeToYaml(Fix(config))
-            });
-        }
     }
 
-    static ComponentConfig Fix(ComponentConfig config)
-    {
-        if (config.ExportFilePath.StartsWith("/src/"))
-        {
-            config = config with
-            {
-                DesignLocation = "/" + config.ExportFilePath.RemoveFromStart("/src/").RemoveFromEnd(".tsx")
-            };
-            if (config.ExportFilePath.EndsWith(config.Name + ".tsx"))
-            {
-                config = config with
-                {
-                    ExportFilePath = null,
-
-                    OutputFilePath = "{projectDirectory}/src/{designLocation}.tsx"
-                };
-            }
-            else
-            {
-                throw new NotImplementedException();
-            }
-        }
-        else
-        {
-            throw new NotImplementedException();
-        }
-
-        return config;
-    }
 }
