e5d01e7cc05ae27d5a7d710c84d7188dafe2da8b
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-11-11 01:06:27 +0300

    .

--- ReactWithDotNet.VisualDesigner.Plugins.b-digital/BDigitalSecureConfirm.cs
+++ ReactWithDotNet.VisualDesigner.Plugins.b-digital/BDigitalSecureConfirm.cs
@@ -52,36 +52,30 @@ sealed class BDigitalSecureConfirm : PluginComponentBase
 
     protected override Element render()
     {
-        return new FlexColumn(MarginBottom(24),MarginTop(8))
+        return new div(Id(id), OnClick(onMouseClick), DisplayFlex, FlexDirectionColumn, MarginBottom(24), MarginTop(8), UserSelect(none))
         {
-            new div(FontSize18, FontWeight600, LineHeight32, Color(rgba(0, 0, 0, 0.87))) { "Mobil Onay" },
-                    
-            new FlexRow(Gap(16) ,Background(White), BorderRadius(10), Border(1, solid, "#E0E0E0"), Padding(24), Id(id), OnClick(onMouseClick))
+            new div(FontSize18, FontWeight600, LineHeight32, Color("rgba(0, 0, 0, 0.87)"))
             {
-                new FlexColumn(WidthFull)
+                "Mobil Onay"
+            },
+            new div(Id("0,1"), DisplayFlex, FlexDirectionRow, Gap(16), Background("repeating-linear-gradient(45deg, rgb(253, 224, 71) 0px, rgb(253, 224, 71) 1px, transparent 0px, transparent 50%) 0% 0% / 5px 5px, none 0% 0% repeat scroll padding-box border-box rgb(255, 255, 255)"), BorderRadius(10), Border("1px solid rgb(224, 224, 224)"), Padding(24), Outline("rgb(69, 151, 247) dashed 1px"))
+            {
+                new div(DisplayFlex, FlexDirectionColumn, WidthFull, Outline("rgb(69, 151, 247) dashed 1px"), Background("repeating-linear-gradient(-45deg, rgb(71, 253, 245) 0px, rgb(71, 253, 245) 1px, transparent 0px, transparent 50%) 0% 0% / 5px 5px, none 0% 0% repeat scroll padding-box border-box rgba(0, 0, 0, 0)"))
                 {
-                    new div(FontWeight500)
+                    new div(FontWeight500, Color(rgb(2, 136, 209)))
                     {
-                        "Mobil Onay Bekleniyor...",
-                        Color(rgba(2, 136, 209, 1))
+                        "Mobil Onay Bekleniyor..."
                     },
-                        
                     new div(FontWeight400)
                     {
                         "Ödeme işleminizi gerçekleştirmek için cihazınıza gelen Mobil Onay bildirimine belirtilen süre içerisinde onay vermeniz gerekmektedir."
                     }
                 },
-                        
-                new FlexRowCentered(Size(100))
+                new div(DisplayFlex, FlexDirectionRow, JustifyContentCenter, AlignItemsCenter, Width(100), Height(100), Outline("rgb(69, 151, 247) dashed 1px"), Background("repeating-linear-gradient(-45deg, rgb(71, 253, 245) 0px, rgb(71, 253, 245) 1px, transparent 0px, transparent 50%) 0% 0% / 5px 5px, none 0% 0% repeat scroll padding-box border-box rgba(0, 0, 0, 0)"))
                 {
-                    new FlexRowCentered
+                    new div(DisplayFlex, FlexDirectionRow, JustifyContentCenter, AlignItemsCenter, FontSize24, Color(rgb(2, 136, 209)), Width(64), Height(64), Border("4px solid rgb(2, 136, 209)"), BorderRadius(100))
                     {
-                        "29",
-                        FontSize24,
-                        Color(rgba(2, 136, 209, 1)),
-                        Size(64),
-                        Border(4,solid, rgba(2, 136, 209, 1)),
-                        BorderRadius(100)
+                        "29"
                     }
                 }
             }
