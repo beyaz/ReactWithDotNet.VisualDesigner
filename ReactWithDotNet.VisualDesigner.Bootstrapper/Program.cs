074503563f0cd348ff31d267082c6879b40caf62
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-11-16 13:31:27 +0300

    .

--- ReactWithDotNet.VisualDesigner.Bootstrapper/Program.cs
+++ ReactWithDotNet.VisualDesigner.Bootstrapper/Program.cs
@@ -2,8 +2,8 @@
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
-using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace ReactWithDotNet.VisualDesigner.Bootstrapper;
 
@@ -22,224 +22,127 @@ static class Program
         }
     }
 
+
     static Config CalculateConfig()
     {
-        var config = new Config
+        return new Config
         {
-            VersionUrl           = (string)AppContext.GetData("VersionUrl"),
-            NewVersionZipFileUrl = (string)AppContext.GetData("NewVersionZipFileUrl"),
-            InstallationFolder   = (string)AppContext.GetData("InstallationFolder"),
+            InstallationFolder           = (string)AppContext.GetData(nameof(Config.InstallationFolder)),
+            AppExeFilePath               = (string)AppContext.GetData(nameof(Config.AppExeFilePath)),
+            KillAllNamedProcess          = (string)AppContext.GetData(nameof(Config.KillAllNamedProcess)),
+            DbConnectionString           = (string)AppContext.GetData(nameof(Config.DbConnectionString)),
+            QueryGetLastModificationDate = (string)AppContext.GetData(nameof(Config.QueryGetLastModificationDate)),
+            LocalZipFilePath             = (string)AppContext.GetData(nameof(Config.LocalZipFilePath)),
+            QueryGetFileContent          = (string)AppContext.GetData(nameof(Config.QueryGetFileContent))
         };
-
-        if (config.InstallationFolder is null)
-        {
-            throw new Exception($"Value @{nameof(config.InstallationFolder)} cannot be empty.");
-        }
-
-        if (config.VersionUrl is null)
-        {
-            throw new Exception($"Value @{nameof(config.VersionUrl)} cannot be empty.");
-        }
-
-        if (config.NewVersionZipFileUrl is null)
-        {
-            throw new Exception($"Value @{nameof(config.NewVersionZipFileUrl)} cannot be empty.");
-        }
-
-        return config;
-    }
-
-    static async Task DownloadFileAsync(string url, string localFilePath)
-    {
-        if (IsWebUrl(url))
-        {
-            using var httpClient = new HttpClient();
-
-            await using var fs = new FileStream(localFilePath, FileMode.CreateNew);
-
-            var response = await httpClient.GetAsync(url);
-
-            await response.Content.CopyToAsync(fs);
-
-            return;
-        }
-
-        File.Copy(url, localFilePath);
-    }
-
-    static async Task<string> DownloadStringAsync(string url)
-    {
-        if (IsWebUrl(url))
-        {
-            using var httpClient = new HttpClient();
-
-            return await httpClient.GetStringAsync(url);
-        }
-
-        return await File.ReadAllTextAsync(url);
-    }
-
-    static bool IsWebUrl(string url) => url.StartsWith("https://", StringComparison.OrdinalIgnoreCase);
-
-    static void KillAllNamedProcess(string processName)
-    {
-        foreach (var process in Process.GetProcessesByName(processName))
-        {
-            if (Process.GetCurrentProcess().Id != process.Id)
-            {
-                process.Kill();
-            }
-        }
     }
 
     static async Task Run(Config config)
     {
         Action<string> trace = Console.WriteLine;
 
-        KillAllNamedProcess("ApiInspector.WebUI");
-        KillAllNamedProcess("ApiInspector");
-
-        trace("Checking version...");
-
-        var versionUrl = config.VersionUrl;
-        var newVersionZipFileUrl = config.NewVersionZipFileUrl;
-        var installationFolder = config.InstallationFolder;
-
-        installationFolder = installationFolder.Replace("{MyDocuments}", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
-
-        var shouldUpdate = true;
-
-        trace("Installation Folder: " + installationFolder);
-
-        var localVersionFilePath = Path.Combine(installationFolder, "Version.txt");
-
-        if (File.Exists(localVersionFilePath))
+        // K i l l   N a m e d   P r o c e s s
         {
-            var remoteVersion = await DownloadStringAsync(versionUrl).Then(int.Parse);
-
-            var localVersion = await File.ReadAllTextAsync(localVersionFilePath).Then(int.Parse);
-
-            trace($"Remote Version: {remoteVersion}");
-            trace($"Local Version : {localVersion}");
-
-            if (remoteVersion <= localVersion)
+            foreach (var processName in config.KillAllNamedProcess.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
             {
-                shouldUpdate = false;
+                trace($"Killing process: {processName}");
+            
+                foreach (var process in Process.GetProcessesByName(processName))
+                {
+                    if (Process.GetCurrentProcess().Id != process.Id)
+                    {
+                        process.Kill();
+                    }
+                }
             }
         }
 
-        if (shouldUpdate)
+        // D o w n l o a d   L a t e s t   V e r s i o n
         {
-            trace("Updating to new version...");
+            trace("Checking version...");
+        
+            var localZipFileCreationDate = File.Exists(config.LocalZipFilePath) ? File.GetCreationTime(config.LocalZipFilePath) : DateTime.MinValue;
 
-            if (Directory.Exists(installationFolder))
+            await using var connection = new SqlConnection(config.DbConnectionString);
+            
+            connection.Open();
+            
+            await using (var command = new SqlCommand(config.QueryGetLastModificationDate, connection))
             {
-                trace($"Deleting previous version...{installationFolder}");
-                var isDeleted = await TryDeleteDirectory(installationFolder);
-                if (!isDeleted)
+                var  dbValue = await command.ExecuteScalarAsync();
+
+                var lastModificationDate = Convert.ToDateTime(dbValue);
+
+                if (localZipFileCreationDate > lastModificationDate)
                 {
-                    trace($"Please remove folder manually. {installationFolder}");
-                    Console.Read();
+                    trace("Already using latest version.");
+                    
                     return;
                 }
-
-                trace("Previous version successfully removed.");
             }
-
-            Directory.CreateDirectory(installationFolder);
-
-            var localZipFilePath = Path.Combine(installationFolder, "Remote.zip");
-            if (File.Exists(localZipFilePath))
+            
+            trace("Getting latest version.");
+            
+            await using (var command = new SqlCommand(config.QueryGetFileContent, connection))
             {
-                trace("Clearing zip file...");
-                File.Delete(localZipFilePath);
-            }
-
-            trace($"Downloading... {newVersionZipFileUrl}");
-
-            await DownloadFileAsync(newVersionZipFileUrl, localZipFilePath);
-
-            trace("Extracting...");
-
-            ZipFile.ExtractToDirectory(localZipFilePath, installationFolder, true);
-
-            File.Delete(localZipFilePath);
-
-            var remoteVersion = await DownloadStringAsync(versionUrl).Then(int.Parse);
-
-            await File.WriteAllTextAsync(localVersionFilePath, remoteVersion.ToString());
-        }
-
-        StartWebApplication(installationFolder);
-    }
-
-    static void StartWebApplication(string appFolder)
-    {
-        Console.WriteLine("Starting...");
-
-        var filePath = Path.Combine(appFolder, "ApiInspector.WebUI.exe");
-
-        var processStartInfo = new ProcessStartInfo(filePath)
-        {
-            WorkingDirectory = appFolder
-        };
-
-        var process = Process.Start(processStartInfo);
-
-        if (process?.HasExited == true)
-        {
-            Console.WriteLine($"Exieted with exitCode: {process.ExitCode}");
-        }
-        else
-        {
-            Console.WriteLine("Started.");
-        }
-    }
-
-    static async Task<B> Then<A, B>(this Task<A> task, Func<A, B> nextFunc)
-    {
-        var a = await task;
-
-        return nextFunc(a);
-    }
-
-    static async Task<bool> TryDeleteDirectory(string directoryPath, int maxRetries = 10, int millisecondsDelay = 30)
-    {
-        if (directoryPath == null)
-            throw new ArgumentNullException(nameof(directoryPath));
-        if (maxRetries < 1)
-            throw new ArgumentOutOfRangeException(nameof(maxRetries));
-        if (millisecondsDelay < 1)
-            throw new ArgumentOutOfRangeException(nameof(millisecondsDelay));
-
-        for (var i = 0; i < maxRetries; ++i)
-        {
-            try
-            {
-                if (Directory.Exists(directoryPath))
+                var  bytes = (byte[])await command.ExecuteScalarAsync();
+                if (bytes is null)
                 {
-                    Directory.Delete(directoryPath, true);
+                    throw new Exception("File content not fetched. Zero bytes fetched.");
                 }
+                
+                trace($"Saving local file: {config.LocalZipFilePath}");
+                
+                await File.WriteAllBytesAsync(config.LocalZipFilePath, bytes);
+            }
 
-                return true;
-            }
-            catch (IOException)
-            {
-                await Task.Delay(millisecondsDelay);
-            }
-            catch (UnauthorizedAccessException)
-            {
-                await Task.Delay(millisecondsDelay);
-            }
+            await connection.CloseAsync();
         }
 
-        return false;
+        // E x t r a c t   L o c a l   z i p   F i l e
+        {
+            trace("Started to extract zip file...");
+            ZipFile.ExtractToDirectory(config.LocalZipFilePath, config.InstallationFolder, true);
+            trace("Zip file extracted");
+        }
+        
+
+
+        // S t a r t   A p p l i c a t i o n
+        {
+            trace($"Starting application: {config.AppExeFilePath}");
+            
+            var processStartInfo = new ProcessStartInfo(config.AppExeFilePath)
+            {
+                WorkingDirectory = Path.GetDirectoryName(config.AppExeFilePath) ?? string.Empty
+            };
+
+            var process = Process.Start(processStartInfo);
+
+            if (process?.HasExited == true)
+            {
+                throw new Exception($"Exieted with exitCode: {process.ExitCode}");
+            }
+
+            trace("Started.");
+        }
     }
 
     sealed class Config
     {
         public string InstallationFolder { get; init; }
-        public string NewVersionZipFileUrl { get; init; }
-        public string VersionUrl { get; init; }
+        
+        public string AppExeFilePath { get; init; }
+       
+        public string KillAllNamedProcess { get; init; }
+        
+        public string DbConnectionString { get; init; }
+        
+        public string QueryGetLastModificationDate { get; init; }
+        
+        public string LocalZipFilePath  { get; init; }
+        
+        public string QueryGetFileContent  { get; init; }
+        
     }
 }
