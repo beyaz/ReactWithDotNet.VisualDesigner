8fe1371522c0403edff275760c51e11b2a3fad21
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-11-16 22:29:25 +0300

    .

--- Plugins/ReactWithDotNet.VisualDesigner.Plugins.b-digital/BTabBar.cs
+++ Plugins/ReactWithDotNet.VisualDesigner.Plugins.b-digital/BTabBar.cs
@@ -28,16 +28,59 @@ sealed class BTabBar : PluginComponentBase
 
     
     [NodeAnalyzer]
-    public static NodeAnalyzeOutput AnalyzeReactNode(NodeAnalyzeInput input)
+    public static async NodeAnalyzeOutput AnalyzeReactNode(NodeAnalyzeInput input)
     {
         if (input.Node.Tag != nameof(BTabBar))
         {
-            return AnalyzeChildren(input, AnalyzeReactNode);
+            return await AnalyzeChildren(input, AnalyzeReactNode);
         }
         
-        var (node, componentConfig) = input;
+        var node = input.Node;
 
-        return Result.From(node);
+        foreach (var name in new[] { "leftListData", "rightListData" })
+        {
+            var listData = node.TryFindDesignNamedNode(name);
+            if (listData is null)
+            {
+                continue;
+            }
+
+            IReadOnlyList<string> linesCollection;
+            {
+                var response = await
+                (
+                    from child in listData.Children
+                    from analyzedChild in input.AnalyzeNode(child)
+                    from lines in input.ReactNodeModelToElementTreeSourceLinesConverter(analyzedChild)
+                    select string.Join(Environment.NewLine, lines)
+                ).AsResult();
+
+                if (response.HasError)
+                {
+                    return response.Error;
+                }
+
+                linesCollection = response.Value;
+            }
+
+            var items = string.Join("," + Environment.NewLine, linesCollection);
+
+            var property = new ReactProperty
+            {
+                Name  = name,
+                Value = "[" + items + "]"
+            };
+
+            node = node with
+            {
+                Properties = node.Properties.Add(property)
+            };
+        }
+
+        return node with
+        {
+            Children = []
+        };
     }
 
   
