7a1eff80489bffa6bb901862fba9e037b8977251
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-11-20 00:08:33 +0300

    .

--- TsxFormatter/server.js
+++ TsxFormatter/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const prettier = require('prettier');
+const ts = require('typescript');
 
 const app = express();
 const port = 5009;
 
 app.use(express.json({ limit: '4mb' }));
 
+// ------------------------
+// Prettier endpoint
+// ------------------------
 app.post('/format', async (req, res) => 
 {
   const code = req.body.code;
@@ -35,6 +39,32 @@ app.post('/format', async (req, res) =>
   catch (err) 
   {
     res.status(500).json({ error: "FormattingErrorOccurred", details: err.message });
+  }
+});
+
+
+// ------------------------
+// TSX AST endpoint
+// ------------------------
+app.post('/ast', (req, res) => 
+{
+  const code = req.body.code;
+
+  if (!code) return res.status(400).json({ error: "EmptyRequest" });
+
+  try {
+    const ast = ts.createSourceFile(
+      'file.tsx',
+      code,
+      ts.ScriptTarget.Latest,
+      true,
+      ts.ScriptKind.TSX
+    );
+
+    // AST JSON olarak döndür
+    res.status(200).json(ast);
+  } catch (err) {
+    res.status(500).json({ error: "AstGenerationError", details: err.message });
   }
 });
 
