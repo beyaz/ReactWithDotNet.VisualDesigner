ee1d96696ce9648531cc6f1e6f89f6ea3b52221c
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-12-16 17:56:28 +0300

    .

---
+++ Plugins/ReactWithDotNet.VisualDesigner.Plugins.b-digital/BInputNumericExtended.cs
@@ -0,0 +1,146 @@
+﻿namespace ReactWithDotNet.VisualDesigner.Plugins.b_digital;
+
+[CustomComponent]
+sealed class BInputNumericExtended : PluginComponentBase
+{
+    [JsTypeInfo(JsType.String)]
+    public string errorText { get; set; }
+    
+    [JsTypeInfo(JsType.String)]
+    public string floatingLabelText { get; set; }
+
+    [JsTypeInfo(JsType.String)]
+    public string helperText { get; set; }
+    
+    [JsTypeInfo(JsType.Boolean)]
+    public string isRequired { get; set; }
+
+    [JsTypeInfo(JsType.Number)]
+    public string maxLength { get; set; }
+
+    [JsTypeInfo(JsType.Function)]
+    public string onChange { get; set; }
+
+    [JsTypeInfo(JsType.String)]
+    public string value { get; set; }
+
+    [JsTypeInfo(JsType.Boolean)]
+    public string disabled { get; set; }
+
+    [NodeAnalyzer]
+    public static NodeAnalyzeOutput AnalyzeReactNode(NodeAnalyzeInput input)
+    {
+        if (input.Node.Tag != nameof(BInputNumericExtended))
+        {
+            return AnalyzeChildren(input, AnalyzeReactNode);
+        }
+
+        var node = input.Node;
+
+        node = ApplyTranslateOperationOnProps(node, input.ComponentConfig, nameof(floatingLabelText));
+
+
+        var valueProp = node.Properties.FirstOrDefault(x => x.Name == nameof(value));
+        var onChangeProp = node.Properties.FirstOrDefault(x => x.Name == nameof(onChange));
+        var isRequiredProp = node.Properties.FirstOrDefault(x => x.Name == nameof(isRequired));
+
+        if (valueProp is not null)
+        {
+            var properties = node.Properties;
+
+            var lines = new TsLineCollection
+            {
+                "(e: any, value: any) =>",
+                "{",
+                GetUpdateStateLines(valueProp.Value, "value")
+            };
+
+            if (onChangeProp is not null)
+            {
+                if (IsAlphaNumeric(onChangeProp.Value))
+                {
+                    lines.Add(onChangeProp.Value + "(e, value);");
+                }
+                else
+                {
+                    lines.Add(onChangeProp.Value);
+                }
+            }
+
+            lines.Add("}");
+
+            if (onChangeProp is not null)
+            {
+                onChangeProp = onChangeProp with
+                {
+                    Value = lines.ToTsCode()
+                };
+
+                properties = properties.SetItem(properties.FindIndex(x => x.Name == onChangeProp.Name), onChangeProp);
+            }
+            else
+            {
+                properties = properties.Add(new()
+                {
+                    Name = "onChange",
+                    Value = lines.ToTsCode()
+                });
+            }
+
+            node = node with { Properties = properties };
+        }
+
+        if (isRequiredProp is not null)
+        {
+            
+
+            node = node with
+            {
+                Properties = node.Properties.Remove(isRequiredProp).Add(new()
+                {
+                    Name = "valueConstraint",
+                    Value = $"{{ required: {Plugin.ConvertDotNetPathToJsPath(isRequiredProp.Value)} }}"
+                })
+            };
+        }
+
+        node = AddContextProp(node);
+        
+        return Result.From((node, new TsImportCollection
+        {
+            {nameof(BInputNumericExtended),"b-input-numeric-extended"}
+        }));
+    }
+
+    protected override Element render()
+    {
+        var textContent = string.Empty;
+        if (floatingLabelText.HasValue)
+        {
+            textContent = floatingLabelText;
+        }
+
+        if (value.HasValue)
+        {
+            textContent += " | " + value;
+        }
+
+        return new div(PaddingTop(16), PaddingBottom(8))
+        {
+            new FlexRow(AlignItemsCenter, PaddingLeftRight(16), Border(1, solid, "#c0c0c0"), BorderRadius(10), Height(58), JustifyContentSpaceBetween)
+            {
+                new div(Color(rgba(0, 0, 0, 0.54)), FontSize16, FontWeight400, FontFamily("Roboto, sans-serif"))
+                {
+                    textContent
+                },
+
+                Id(id), OnClick(onMouseClick)
+            },
+            new FlexRow(JustifyContentSpaceBetween, FontSize12, PaddingLeftRight(14), Color(rgb(158, 158, 158)), LineHeight15)
+            {
+                new div { helperText },
+                new div { maxLength }
+            }
+        };
+    }
+}
