952f630ce738cc2264b73e4ae42792504e47b554
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-12-17 17:17:42 +0300

    .

--- Plugins/ReactWithDotNet.VisualDesigner.Plugins.b-digital/BInputNumericExtended.cs
+++ Plugins/ReactWithDotNet.VisualDesigner.Plugins.b-digital/BInputNumericExtended.cs
@@ -26,7 +26,25 @@ sealed class BInputNumericExtended : PluginComponentBase
 
     [JsTypeInfo(JsType.Boolean)]
     public string disabled { get; set; }
+    
+    [JsTypeInfo(JsType.String)]
+    public string hintText { get; set; }
 
+    [Suggestions("left , center , right")]
+    [JsTypeInfo(JsType.String)]
+    public string inputAlign { get; set; }
+
+    [Suggestions("F , M, D")]
+    [JsTypeInfo(JsType.String)]
+    public string format { get; set; }
+    
+    [JsTypeInfo(JsType.Number)]
+    public string minValue { get; set; }
+    
+    [JsTypeInfo(JsType.Number)]
+    public string maxValue { get; set; }
+    
+   
     [NodeAnalyzer]
     public static NodeAnalyzeOutput AnalyzeReactNode(NodeAnalyzeInput input)
     {
@@ -114,27 +132,49 @@ sealed class BInputNumericExtended : PluginComponentBase
 
     protected override Element render()
     {
-        var textContent = string.Empty;
-        if (floatingLabelText.HasValue)
-        {
-            textContent = floatingLabelText;
-        }
-
-        if (value.HasValue)
-        {
-            textContent += " | " + value;
-        }
-
         return new div(PaddingTop(16), PaddingBottom(8))
         {
-            new FlexRow(AlignItemsCenter, PaddingLeftRight(16), Border(1, solid, "#c0c0c0"), BorderRadius(10), Height(58), JustifyContentSpaceBetween)
+            Id(id), OnClick(onMouseClick),
+            
+            new FlexRow(AlignItemsCenter, PaddingLeftRight(16), Border(1, solid, "#c0c0c0"), BorderRadius(10), Height(58))
             {
+                // L a b e l   o n   t o p - l e f t   b o r d e r 
+                PositionRelative,
+                new label
+                {
+                    // c o n t e n t
+                    floatingLabelText,
+                    
+                    // l a y o u t
+                    PositionAbsolute,
+                    Top(-6),
+                    Left(16),
+                    PaddingX(4),
+                    
+                    // t h e m e
+                    Color(rgba(0, 0, 0, 0.6)),
+                    FontSize12,
+                    FontWeight400,
+                    LineHeight12,
+                    LetterSpacing(0.15),
+                    FontFamily("Roboto"),
+                    Background(White)
+                },
                 new div(Color(rgba(0, 0, 0, 0.54)), FontSize16, FontWeight400, FontFamily("Roboto, sans-serif"))
                 {
-                    textContent
+                    value
                 },
-
-                Id(id), OnClick(onMouseClick)
+                
+                inputAlign switch
+                {
+                    "left"=> JustifyContentFlexStart,
+                    
+                    "right"=> JustifyContentFlexEnd,
+                    
+                    "center"=> JustifyContentCenter,
+                    
+                    _=> JustifyContentFlexEnd
+                }
             },
             new FlexRow(JustifyContentSpaceBetween, FontSize12, PaddingLeftRight(14), Color(rgb(158, 158, 158)), LineHeight15)
             {
