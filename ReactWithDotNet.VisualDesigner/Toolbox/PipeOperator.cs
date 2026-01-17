4fff1578b64669f0240f28e60162adc2d40c1854
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-01-17 21:15:48 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/Toolbox/PipeOperator.cs
@@ -0,0 +1,12 @@
+﻿namespace Toolbox;
+
+public static class PipeOperator
+{
+    extension<Tin, Tout>(Tin)
+    {
+        public static Tout operator | (Tin source, Func<Tin, Tout> func)
+        {
+            return func(source);
+        }
+    }
+}
