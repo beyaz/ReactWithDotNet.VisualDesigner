877c0098c0e0a75bdb6455ef5db6fd223c8c8271
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-01-24 12:13:44 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/Toolbox/TupleLinqExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Toolbox;
+
+/// <summary>
+///     Provides LINQ-style extension methods for working with tuples and other types.
+/// </summary>
+public static class TupleLinqExtensions
+{
+    // from x in value
+    public static T Select<T>(this T source, Func<T, T> selector)
+    {
+        return selector(source);
+    }
+
+    // select
+    public static TResult Select<T, TResult>(this T source, Func<T, TResult> selector)
+    {
+        return selector(source);
+    }
+
+    // from x in ...
+    // from y in ...
+    public static TResult SelectMany<T, TIntermediate, TResult>(this T source, Func<T, TIntermediate> binder, Func<T, TIntermediate, TResult> projector)
+    {
+        var intermediate = binder(source);
+
+        return projector(source, intermediate);
+    }
+}
