49322d26bffc004b9f5ff6addab926c801d54b43
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-01-24 13:28:00 +0300

    .

--- ReactWithDotNet.VisualDesigner/Toolbox/TupleLinqExtensions.cs
+++ ReactWithDotNet.VisualDesigner/Toolbox/TupleLinqExtensions.cs
@@ -5,14 +5,6 @@
 /// </summary>
 public static class TupleLinqExtensions
 {
-    // from x in value
-    public static T Select<T>(this T source, Func<T, T> selector)
-    {
-        return selector(source);
-    }
-
-    // select
-
     public static TResult Select<T1, T2, TResult>(this (T1, T2) source, Func<(T1, T2), TResult> selector)
     {
         return selector(source);
@@ -31,14 +23,5 @@ public static class TupleLinqExtensions
     public static TResult Select<T1, T2, T3, T4, T5, TResult>(this (T1, T2, T3, T4, T5) source, Func<(T1, T2, T3, T4, T5), TResult> selector)
     {
         return selector(source);
-    }
-
-    // from x in ...
-    // from y in ...
-    public static TResult SelectMany<T, TIntermediate, TResult>(this T source, Func<T, TIntermediate> binder, Func<T, TIntermediate, TResult> projector)
-    {
-        var intermediate = binder(source);
-
-        return projector(source, intermediate);
     }
 }
