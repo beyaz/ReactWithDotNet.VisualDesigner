43821a35068ac1b7c92e860f92a0c287fc46440b
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-02-23 00:35:15 +0300

    .

--- ReactWithDotNet.VisualDesigner/TsxAnalyze/SuggestionFromTsxCode.cs
+++ ReactWithDotNet.VisualDesigner/TsxAnalyze/SuggestionFromTsxCode.cs
@@ -9,67 +9,18 @@ static class SuggestionFromTsxCode
     {
         var list = new List<string>();
 
-        if (tsxFilePath is null || !File.Exists(tsxFilePath))
+        var (success, directoryInfo) = TryNavigatePackagesDotJsonFolder(tsxFilePath);
+        if (success)
         {
-            return list;
-        }
-
-        var fileContent = await File.ReadAllTextAsync(tsxFilePath);
-
-        foreach (var file in CollectRelatedFilePaths(tsxFilePath, fileContent))
-        {
-            var suggestionsFromRelativeFile = await GetAllVariableSuggestionsInFile(file);
-            if (suggestionsFromRelativeFile.HasError)
+            foreach (var fileInfo in from f in directoryInfo.GetFiles("*", SearchOption.AllDirectories) where f.Extension is ".ts" or ".tsx" select f)
             {
-                return suggestionsFromRelativeFile.Error;
+                var fileContent = await File.ReadAllTextAsync(fileInfo.FullName);
+
+                list.AddRange(CalculateVariableSuggestions(fileContent));
             }
-
-            list.Add(suggestionsFromRelativeFile.Value);
         }
-
-        list.AddRange(CalculateVariableSuggestions(fileContent));
 
         return list.Where(x => x.Length > 1).Distinct().ToList();
-
-        static IReadOnlyList<string> CollectRelatedFilePaths(string tsxFilePath, string fileContent)
-        {
-            List<string> collectedFiles = [];
-
-            foreach (var line in fileContent.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
-            {
-                // is relative path in project
-                if (line.Contains('"' + "../"))
-                {
-                    const string pathPattern = """
-                                               "([^"]+)"
-                                               """;
-
-                    var match = Regex.Match(line, pathPattern);
-                    if (match.Success)
-                    {
-                        var relativeFolderPath = match.Groups[1].Value;
-
-                        relativeFolderPath = Path.Combine(Path.GetDirectoryName(tsxFilePath)!, relativeFolderPath);
-
-                        relativeFolderPath = Path.GetFullPath(relativeFolderPath);
-
-                        if (File.Exists(relativeFolderPath + ".ts"))
-                        {
-                            collectedFiles.Add(relativeFolderPath + ".ts");
-                        }
-                        else if (Directory.Exists(relativeFolderPath))
-                        {
-                            foreach (var file in Directory.GetFiles(relativeFolderPath, "*.ts", SearchOption.TopDirectoryOnly))
-                            {
-                                collectedFiles.Add(file);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return collectedFiles;
-        }
     }
 
     static IReadOnlyList<string> CalculateVariableSuggestions(string tsxCode)
@@ -107,5 +58,35 @@ static class SuggestionFromTsxCode
         }
 
         return new List<string>(suggestions);
+    }
+
+    static (bool success, DirectoryInfo directoryInfo) TryNavigatePackagesDotJsonFolder(string tsxFilePath)
+    {
+        var directoryPath = Path.GetDirectoryName(tsxFilePath);
+        if (string.IsNullOrWhiteSpace(directoryPath))
+        {
+            return (false, null);
+        }
+
+        var directoryInfo = new DirectoryInfo(directoryPath);
+
+        while (directoryInfo.Exists)
+        {
+            var parent = directoryInfo.Parent;
+            if (parent is null || parent.Exists is false)
+            {
+                return (false, null);
+            }
+
+            var packages_json = Path.Combine(parent.FullName, "package.json");
+            if (File.Exists(packages_json))
+            {
+                return (true, directoryInfo);
+            }
+
+            directoryInfo = parent;
+        }
+
+        return (false, null);
     }
 }
