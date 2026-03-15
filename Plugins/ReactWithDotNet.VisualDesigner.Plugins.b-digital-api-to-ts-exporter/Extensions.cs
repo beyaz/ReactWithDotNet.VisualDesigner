e84a066ef6f45ed4543e3363aeff820c2c175693
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-03-16 00:12:28 +0300

    .

--- Plugins/ReactWithDotNet.VisualDesigner.Plugins.b-digital-api-to-ts-exporter/Extensions.cs
+++ Plugins/ReactWithDotNet.VisualDesigner.Plugins.b-digital-api-to-ts-exporter/Extensions.cs
@@ -1,4 +1,8 @@
-﻿using Mono.Cecil;
+﻿using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Mono.Cecil;
 using Newtonsoft.Json.Serialization;
 
 namespace BDigitalFrameworkApiToTsExporter;
@@ -109,6 +113,25 @@ static class Extensions
 
         public bool IsObject
             => typeReference.FullName == typeof(object).FullName;
+
+        public bool IsNullable => typeReference.Name == "Nullable`1" && typeReference.IsGenericInstance;
+
+        public bool IsCollectionType
+        {
+            get
+            {
+                var names = new List<string>
+                {
+                    typeof(Collection<>).FullName,
+                    typeof(IReadOnlyCollection<>).FullName,
+                    typeof(List<>).FullName,
+                    typeof(IReadOnlyList<>).FullName,
+                    typeof(ImmutableList<>).FullName
+                };
+
+                return names.Any(name => typeReference.FullName.StartsWith(name, StringComparison.OrdinalIgnoreCase));
+            }
+        }
     }
 
     extension(PropertyDefinition propertyDefinition)
@@ -163,6 +186,87 @@ static class Extensions
                 bool HasCustomAttributeNameLike(string attributeName)
                 {
                     return propertyDefinition.CustomAttributes.Any(x => x.AttributeType.Name.Contains(attributeName, StringComparison.OrdinalIgnoreCase));
+                }
+            }
+        }
+
+        public bool IsNullable
+        {
+            get
+            {
+                // is value type
+                if (isSystemNullable(propertyDefinition.PropertyType))
+                {
+                    return true;
+                }
+
+                if (hasNullableAttribute(propertyDefinition, NullabilityState.Nullable))
+                {
+                    return true;
+                }
+
+                if (hasNullableContextAttribute(propertyDefinition, NullabilityState.Nullable))
+                {
+                    if (propertyDefinition.PropertyType.IsValueType)
+                    {
+                        return false;
+                    }
+
+                    if (hasNullableAttribute(propertyDefinition, NullabilityState.NotNull))
+                    {
+                        return false;
+                    }
+
+                    return true;
+                }
+
+                return false;
+
+                static bool isSystemNullable(TypeReference typeReference)
+                {
+                    return typeReference is GenericInstanceType git && git.ElementType.FullName == "System.Nullable`1";
+                }
+
+                static bool hasNullableAttribute(PropertyDefinition propertyDefinition, NullabilityState state)
+                {
+                    // Reference type nullability (C# 8)
+                    var nullableAttribute = propertyDefinition.CustomAttributes.FirstOrDefault(a => a.AttributeType.FullName == typeof(NullableAttribute).FullName);
+                    if (nullableAttribute is null)
+                    {
+                        return false;
+                    }
+
+                    // 2:nullable 1:nonnullable 0: oblivious
+                    var argument = nullableAttribute.ConstructorArguments[0];
+                    if (argument.Type.FullName == "System.Byte")
+                    {
+                        return (byte)argument.Value == (byte)state;
+                    }
+
+                    if (argument.Type.FullName == "System.Byte[]")
+                    {
+                        return ((CustomAttributeArgument[])argument.Value)[0].Value is byte b && b == (byte)state;
+                    }
+
+                    return false;
+                }
+
+                static bool hasNullableContextAttribute(PropertyDefinition propertyDefinition, NullabilityState state)
+                {
+                    // NullableContextAttribute class / module seviyesinde olabilir
+                    var nullableContext = propertyDefinition.DeclaringType.CustomAttributes
+                                              .FirstOrDefault(a => a.AttributeType.FullName == typeof(NullableContextAttribute).FullName)
+                                          ?? propertyDefinition.Module.CustomAttributes
+                                              .FirstOrDefault(a => a.AttributeType.FullName == typeof(NullableContextAttribute).FullName);
+
+                    if (nullableContext == null)
+                    {
+                        return false;
+                    }
+
+                    var argument = (byte)nullableContext.ConstructorArguments[0].Value;
+
+                    return argument == (byte)state;
                 }
             }
         }
