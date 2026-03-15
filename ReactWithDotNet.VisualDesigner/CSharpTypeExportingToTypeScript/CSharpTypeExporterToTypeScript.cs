6759bc42c4e1746155051c0adc484a02fe586fcf
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-03-15 23:49:59 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/CSharpTypeExportingToTypeScript/CSharpTypeExporterToTypeScript.cs
@@ -0,0 +1,29 @@
+﻿namespace ReactWithDotNet.VisualDesigner.CSharpTypeExportingToTypeScript;
+
+/*
+ * ExportTypeScriptFromCSharp
+    variables:
+      - ApiDll: 'c:\dd.c.dll'
+      - out: d:\boa\tools\
+      - ns: BOA.Common.X.Api
+     
+   
+    types:
+      - type: {ns}.Models.XModel1
+        output: {out}{TypeName}.ts
+   
+      - type: {ns}.Types.User*
+        output: {out}{TypeName}.ts
+   	 
+      - type: {ns}.Types.User
+        output: {out}{TypeName}.ts
+   	 includeOnlyProperties:[ 'abc', 'yx']
+   	 
+      - type: {ns}.Types.User
+        output: {out}{TypeName}.ts
+   	 excludeProperties:[ 'abc', 'yx']
+ */
+class CSharpTypeExporterToTypeScript
+{
+    
+}
