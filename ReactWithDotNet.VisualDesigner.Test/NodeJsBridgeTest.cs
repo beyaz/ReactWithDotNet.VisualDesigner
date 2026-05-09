04ffb625ff91a84c50e775eb1fcff914a7492c37
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-05-09 14:31:56 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner.Test/NodeJsBridgeTest.cs
@@ -0,0 +1,21 @@
+﻿namespace ReactWithDotNet.VisualDesigner.Test;
+
+[TestClass]
+public class NodeJsBridgeTest
+{
+    [TestMethod]
+    public async Task KebabToCamelCaseTest()
+    {
+        var ast = await NodeJsBridge.Ast("""
+                                         const x = 5;");
+
+                                         return  (
+                                           <div>
+                                             <h1>Hello, world!</h1>
+                                             </div>
+                                         );
+                                         """);
+
+        ast.Value.ToString();
+    }
+}
