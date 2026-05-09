803c61b85b403cb7870dd37c3b46904e48c8892b
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-05-09 23:29:38 +0300

    .

---
+++ ReactWithDotNet.VisualDesigner/JsxPreview/3 Jsx Model.cs
@@ -0,0 +1,27 @@
+﻿namespace ReactWithDotNet.VisualDesigner.JsxPreview;
+
+sealed class JsxElementDto
+{
+    // @formatter:off
+        
+    public string Tag { get; set; }
+    
+    public List<string> Props { get; set; } = [];
+    
+    public List<JsxElementDto> Children { get; set; } = [];
+    
+    public string Condition { get; set; }
+    
+    // @formatter:on
+}
+
+sealed class MethodResult
+{
+    // @formatter:off
+      
+    public string MethodName { get; set; }
+      
+    public List<JsxElementDto> Elements { get; set; } = [];
+
+    // @formatter:on
+}
